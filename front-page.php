67c3e371f2f4bedebcb46e8eb9172a45e1bd9849
kurudrive <kurudrive@gmail.com>
2015-06-11 18:43:35 +0900

    固定ページをトップにされた場合の処理

--- front-page.php
+++ front-page.php
@@ -1,29 +1,44 @@
 <?php get_header(); ?>
-<div class="section sectionBox siteContent" id="top__blog">
+<div class="section siteContent">
     <div class="container">
-
         <div class="row">
-            <div class="col-md-12 postList">
+            <div class="col-md-12">
 
 <?php if (have_posts()) : ?>
 
-    <div class="postList">
+    <?php if ( 'page' == get_option('show_on_front') ) : ?>
 
         <?php while ( have_posts() ) : the_post();?>
 
-            <?php get_template_part('module_post__loop_item'); ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <div class="entry-body">
+            <?php the_content(); ?>
+            </div>
+            <?php wp_link_pages( array( 'before' => '<div class="page-link">' . 'Pages:', 'after' => '</div>' ) ); ?>
+             </article><!-- [ /#post-<?php the_ID(); ?> ] -->
 
         <?php endwhile;?>
 
-    </div>
+    <?php else : ?>
 
-    <?php the_posts_pagination(array (
-                            'mid_size'  => 1,
-                            'prev_text' => __ ( '&laquo;', 'bvII' ),
-                            'next_text' => __ ( '&raquo;', 'bvII' ),
-                            'type'      => 'list',
-                            'before_page_number' => '<span class="meta-nav screen-reader-text">' . __ ( 'Page', 'bvII' ) . ' </span>'
-                        ) ); ?>
+        <div class="postList">
+
+            <?php while ( have_posts() ) : the_post();?>
+
+                <?php get_template_part('module_post__loop_item'); ?>
+
+            <?php endwhile;?>
+            <?php the_posts_pagination(array (
+                                    'mid_size'  => 1,
+                                    'prev_text' => __ ( '&laquo;', 'bvII' ),
+                                    'next_text' => __ ( '&raquo;', 'bvII' ),
+                                    'type'      => 'list',
+                                    'before_page_number' => '<span class="meta-nav screen-reader-text">' . __ ( 'Page', 'bvII' ) . ' </span>'
+                                ) ); ?>
+
+        </div><!-- [ /.postList ] -->
+
+    <?php endif; ?>
 
 <?php else: ?>
 
@@ -32,7 +47,7 @@
 <?php endif; // have_post() ?>
 
             </div>
-        </div>
-    </div>
-</div>
+        </div><!-- [ /.row ] -->
+    </div><!-- [ /.container ] -->
+</div><!-- [ /.siteContent ] -->
  <?php get_footer(); ?>
