b16f873bf6b3a9b02813eabb43c0e6ef109aa221
kurudrive <kurudrive@gmail.com>
2015-06-23 16:21:39 +0900

    [ 不具合修正 ] タイトルが空でも詳細ページにリンク出来るように

--- module_loop_post.php
+++ module_loop_post.php
@@ -12,7 +12,7 @@
 	<div class="media-body">
 		<?php get_template_part('module_loop_post_meta');?>
 		<h4 class="media-heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-		<?php the_excerpt(); ?>
+		<a href="<?php the_permalink(); ?>" class="media-body_excerpt"><?php the_excerpt(); ?></a>
 		<!--
 		<div><a href="<?php the_permalink(); ?>" class="btn btn-default btn-sm"><?php _e('Read more', 'lightning'); ?></a></div>
 		-->   
