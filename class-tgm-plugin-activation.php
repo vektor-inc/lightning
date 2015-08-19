cb9ec4c5e9a295d69717b65208f604f05d3d2af4
kurudrive <kurudrive@gmail.com>
2015-08-19 13:04:56 +0900

    [ Theme check measures ]

--- class-tgm-plugin-activation.php
+++ class-tgm-plugin-activation.php
@@ -634,11 +634,9 @@ if ( ! class_exists( 'TGM_Plugin_Activation' ) ) {
 			if ( has_filter( 'tgmpa_admin_menu_use_add_theme_page' ) ) {
 				_deprecated_function( 'The "tgmpa_admin_menu_use_add_theme_page" filter', '2.5.0', esc_html__( 'Set the parent_slug config variable instead.', 'lightning' ) );
 			}
-
 			if ( 'themes.php' === $this->parent_slug ) {
+
 				$this->page_hook = call_user_func( 'add_theme_page', $args['page_title'], $args['menu_title'], $args['capability'], $args['menu_slug'], $args['function'] );
-			} else {
-				$this->page_hook = call_user_func( 'add_submenu_page', $args['parent_slug'], $args['page_title'], $args['menu_title'], $args['capability'], $args['menu_slug'], $args['function'] );
 			}
 		}
 
