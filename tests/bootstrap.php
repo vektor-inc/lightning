0d5a090a39270763e0b90a9973cefb65eeedf300
kurudrive <kurudrive@gmail.com>
2017-05-31 11:44:28 +0900

    ci test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,12 +11,7 @@ require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
 	register_theme_directory( dirname( __FILE__ ) . '/../../' );
-	$a = wp_get_theme();
-	print '<pre style="text-align:left">';print_r($a);print '</pre>';
 	switch_theme('lightning');
-	echo '━━━━━━━━━━━━━━━━━━━━'."<br>\n";
-		$a = wp_get_theme();
-	print '<pre style="text-align:left">';print_r($a);print '</pre>';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
