3a27006bcc1ee98b8438e67281b1997d621a0d08
kurudrive <kurudrive@gmail.com>
2018-06-24 19:31:17 +0900

    [ skin system ] スキン新旧判定ロジック変更 #101

--- functions.php
+++ functions.php
@@ -148,47 +148,27 @@ require get_parent_theme_file_path( '/inc/template-tags.php' );
 /*	Load designskin manager
 /*-------------------------------------------*/
 
-function lightning_is_old_skin() {
-	$skin_current    = get_option( 'lightning_design_skin' );
-	$default_headers = array(
-		'Name'        => 'Plugin Name',
-		'PluginURI'   => 'Plugin URI',
-		'Version'     => 'Version',
-		'Description' => 'Description',
-		'Author'      => 'Author',
-		'AuthorURI'   => 'Author URI',
-		'TextDomain'  => 'Text Domain',
-		'DomainPath'  => 'Domain Path',
-		'Network'     => 'Network',
-		// Site Wide Only is deprecated in favor of Network.
-		'_sitewide'   => 'Site Wide Only',
-	);
-
-	$skins = array(
-		'charm' => array(
-			'plugin_path' => WP_PLUGIN_DIR . '/lightning-skin-charm/lightning_skin_charm.php',
-			'new_version' => '3.0.0',
-		),
-	);
-	if ( $skin_current == 'origin' || ! $skin_current ) {
-		return false;
-	} elseif ( file_exists( $skins[ $skin_current ]['plugin_path'] ) ) {
-		// already file name changed = new version!
-		return false;
+function lightning_is_new_skin() {
+	$skin_current = get_option( 'lightning_design_skin' );
+	if ( $skin_current == 'origin' || $skin_current == '' ) {
+		// New Skin System
+		return true;
 	} else {
-		$plugin_data     = get_file_data( $skins[ $skin_current ]['plugin_path'], $default_headers );
-		$version_current = $plugin_data['Version'];
-		$version_limit   = $skins[ $skin_current ]['new_version'];
-		if ( version_compare( $version_current, $version_limit ) < 0 ) {
+		$old_skin_system_functions_url = WP_PLUGIN_DIR . '/lightning-skin-' . $skin_current . '/old-functions/old-skin-system-functions.php';
+		if ( file_exists( $old_skin_system_functions_url ) ) {
+			// New Skin System
 			return true;
+		} else {
+			// Old Skin System
+			return false;
 		}
 	}
-};
+}
 
-if ( lightning_is_old_skin() ) {
-	require get_parent_theme_file_path( '/inc/class-design-manager-old.php' );
-} else {
+if ( lightning_is_new_skin() ) {
 	require get_parent_theme_file_path( '/inc/class-design-manager.php' );
+} else {
+	require get_parent_theme_file_path( '/inc/class-design-manager-old.php' );
 }
 
 
