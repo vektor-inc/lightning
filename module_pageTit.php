72206acae674bcb7b58e700a951449e901852bac
kurudrive <kurudrive@gmail.com>
2018-06-28 01:37:04 +0900

    phpcs

--- module_pageTit.php
+++ module_pageTit.php
@@ -3,7 +3,7 @@
 /*	Set tag weight
 /*-------------------------------------------*/
 $page_for_posts = lightning_get_page_for_posts();
-$postType = lightning_get_post_type();
+$postType       = lightning_get_post_type();
 
 // Use post top page（ Archive title wrap to div ）
 if ( $page_for_posts['post_top_use'] ) {
@@ -12,9 +12,9 @@ if ( $page_for_posts['post_top_use'] ) {
 	} else {
 		$pageTitTag = 'h1';
 	}
-// Don't use post top（　Archive title wrap to h1　）
+	// Don't use post top（　Archive title wrap to h1　）
 } else {
-	if ( !is_single() ) {
+	if ( ! is_single() ) {
 		$pageTitTag = 'h1';
 	} else {
 		$pageTitTag = 'div';
@@ -24,10 +24,10 @@ if ( $page_for_posts['post_top_use'] ) {
 /*-------------------------------------------*/
 /*	Set wrap tags
 /*-------------------------------------------*/
-$pageTitHtml_before = '<div class="section page-header"><div class="container"><div class="row"><div class="col-md-12">'."\n";
-$pageTitHtml_before .= '<'.$pageTitTag.' class="page-header_pageTitle">'."\n";
-$pageTitHtml_after = '</'.$pageTitTag.'>'."\n";
-$pageTitHtml_after .= '</div></div></div></div><!-- [ /.page-header ] -->'."\n";
+$pageTitHtml_before  = '<div class="section page-header"><div class="container"><div class="row"><div class="col-md-12">' . "\n";
+$pageTitHtml_before .= '<' . $pageTitTag . ' class="page-header_pageTitle">' . "\n";
+$pageTitHtml_after   = '</' . $pageTitTag . '>' . "\n";
+$pageTitHtml_after  .= '</div></div></div></div><!-- [ /.page-header ] -->' . "\n";
 
 /*-------------------------------------------*/
 /*	Set display title name
@@ -35,11 +35,11 @@ $pageTitHtml_after .= '</div></div></div></div><!-- [ /.page-header ] -->'."\n";
 $pageTitle = '';
 if ( is_page() || is_attachment() ) {
 	$pageTitle = get_the_title();
-} else if ( is_search() ) {
-	$pageTitle = sprintf(__( 'Search Results for : %s', 'lightning' ), get_search_query() );
-} else if ( is_404() ){
+} elseif ( is_search() ) {
+	$pageTitle = sprintf( __( 'Search Results for : %s', 'lightning' ), get_search_query() );
+} elseif ( is_404() ) {
 	$pageTitle = __( 'Not found', 'lightning' );
-} else if ( is_category() || is_tag() || is_tax() || is_home() || is_author() || is_archive() || is_single() ) {
+} elseif ( is_category() || is_tag() || is_tax() || is_home() || is_author() || is_archive() || is_single() ) {
 
 	// Case of post type == 'post'
 	if ( $postType['slug'] == 'post' ) {
@@ -47,16 +47,16 @@ if ( is_page() || is_attachment() ) {
 		if ( $page_for_posts['post_top_use'] ) {
 			$pageTitle = $page_for_posts['post_top_name'];
 
-		// Case of don't use post top page
+			// Case of don't use post top page
 		} else {
 
-			if ( is_single() ){
+			if ( is_single() ) {
 
 				$taxonomies = get_the_taxonomies();
-				if ($taxonomies){
-					$taxonomy = key( $taxonomies );
-					$taxo_cates  = get_the_terms( get_the_ID(),$taxonomy );
-					$pageTitle	= esc_html($taxo_cates[0]->name);
+				if ( $taxonomies ) {
+					$taxonomy   = key( $taxonomies );
+					$taxo_cates = get_the_terms( get_the_ID(), $taxonomy );
+					$pageTitle  = esc_html( $taxo_cates[0]->name );
 				} else {
 					// Case of no category
 					$pageTitle = $postType['name'];
@@ -65,7 +65,7 @@ if ( is_page() || is_attachment() ) {
 				$pageTitle = get_the_archive_title();
 			}
 		} // if ( $page_for_posts['post_top_use'] ) {
-	// Case of custom post type
+		// Case of custom post type
 	} else {
 		$pageTitle = $postType['name'];
 	}
@@ -78,13 +78,13 @@ $pageTitle = apply_filters( 'lightning_pageTitCustom', $pageTitle );
 $pageTitHtml = $pageTitHtml_before;
 // allow tags
 $allowed_html = array(
-    'i' => array(
-    		'class' => array (),
-    	),
-    'br' => array(),
-    'strong' => array()
+	'i'      => array(
+		'class' => array(),
+	),
+	'br'     => array(),
+	'strong' => array(),
 );
-$pageTitHtml .= wp_kses( $pageTitle,$allowed_html );
+$pageTitHtml .= wp_kses( $pageTitle, $allowed_html );
 $pageTitHtml .= $pageTitHtml_after;
-$pageTitHtml = apply_filters( 'lightning_pageTitHtml', $pageTitHtml );
+$pageTitHtml  = apply_filters( 'lightning_pageTitHtml', $pageTitHtml );
 echo $pageTitHtml;
