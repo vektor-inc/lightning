c2fcde846654c429f7f397106ad8fe62918a3d1c
naoki <n.globe.us@gmail.com>
2018-07-18 14:26:44 +0900

    jQueryをフッターに移動

--- functions.php
+++ functions.php
@@ -439,3 +439,25 @@ function lightning_home_content_hidden( $flag ) {
 	}
 	return $flag;
 }
+
+/*-------------------------------------------*/
+/*  Remove lightning-advanced-unit's function.
+/*-------------------------------------------*/
+add_action( 'init', function() {
+	if ( is_admin() ) {
+		return;
+	}
+	// 現在のバージョンとURIを保存。
+	// CDNを使いたい方は$jquery_srcのURIを変更してもよい。
+	global $wp_scripts;
+	$jquery = $wp_scripts->registered['jquery-core'];
+	var_dump($jquery);
+	$jquery_ver = $jquery->ver;
+	$jquery_src = $jquery->src;
+	// いったん削除
+	wp_deregister_script( 'jquery' );
+	wp_deregister_script( 'jquery-core' );
+	// 登録しなおし
+	wp_register_script( 'jquery', false, ['jquery-core'], $jquery_ver, true );
+	wp_register_script( 'jquery-core', $jquery_src, [], $jquery_ver, true );
+} );
