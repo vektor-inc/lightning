fe4453bccfed148fefa4eda01c7ea81b82ff4590
kurudrive <kurudrive@gmail.com>
2018-08-06 12:45:40 +0900

    [ test ] AdvUnit Deactive 処理テスト修正

--- tests/test-lightning_deactive-plugin.php
+++ tests/test-lightning_deactive-plugin.php
@@ -8,27 +8,28 @@ $ phpunit
 */
 
 class LightningDeactivePluginTest extends WP_UnitTestCase {
-function test_lightning_deactivate_plugin() {
+	function test_lightning_deactivate_plugin() {
 
-	print PHP_EOL;
-	print '------------------------------------' . PHP_EOL;
-	print 'is_lightning_deactivate_plugin' . PHP_EOL;
-	print '------------------------------------' . PHP_EOL;
+		print PHP_EOL;
+		print '------------------------------------' . PHP_EOL;
+		print 'is_lightning_deactivate_plugin' . PHP_EOL;
+		print '------------------------------------' . PHP_EOL;
 
-	/* まずは Advanced Unit を有効化する
-	/*--------------------------------*/
-	$plugin_path    = 'lightning-advanced-unit/lightning_advanced_unit.php';
-	$active_plugins = get_option( 'active_plugins' );
-	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-	if ( ! is_plugin_active( $plugin_path ) ) {
-		$active_plugins[] = 'lightning-advanced-unit/lightning_advanced_unit.php';
+		/* まずは Advanced Unit を有効化する
+		/*--------------------------------*/
+		$plugin_path    = 'lightning-advanced-unit/lightning_advanced_unit.php';
+		$active_plugins = get_option( 'active_plugins' );
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+		if ( ! is_plugin_active( $plugin_path ) ) {
+			$active_plugins[] = 'lightning-advanced-unit/lightning_advanced_unit.php';
+		}
+		update_option( 'active_plugins', $active_plugins );
+
+		lightning_deactivate_plugin( $plugin_path );
+
+		$return = is_plugin_active( $plugin_path );
+
+		$this->assertEquals( false, $return );
+
 	}
-	update_option( 'active_plugins', $active_plugins );
-
-	lightning_deactivate_plugin( $plugin_path );
-
-	$return = is_plugin_active( $plugin_path );
-
-	$this->assertEquals( false, $return );
-
 }
