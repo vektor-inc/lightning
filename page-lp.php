f65e2f25995084d55e332e7afb9f4f84e3c98593
kurudrive <kurudrive@gmail.com>
2019-05-06 00:00:12 +0900

    phpcs

--- page-lp.php
+++ page-lp.php
@@ -10,36 +10,45 @@ get_header(); ?>
 
 <div class="col-md-12 mainSection" id="main" role="main">
 
-    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	<?php
+	if ( have_posts() ) {
+		while ( have_posts() ) :
+			the_post();
+		?>
 
-    <?php
-    global $post;
-    $widget_id = 'lp-widget-'.$post->ID;
-    if ( is_active_sidebar( $widget_id ) ) :
-    	dynamic_sidebar( $widget_id );
-    endif;
+			<?php
+			global $post;
+			$widget_id = 'lp-widget-' . $post->ID;
+			if ( is_active_sidebar( $widget_id ) ) :
+				dynamic_sidebar( $widget_id );
+			endif;
 
-    $content = get_the_content();
-    if ( $content ) : ?>
+			$content = get_the_content();
+			if ( $content ) :
+			?>
 
-    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <div class="entry-body">
-    <?php the_content(); ?>
-    </div>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="entry-body">
+	<?php the_content(); ?>
+	</div>
 	<?php
 	$args = array(
-		'before'           => '<nav class="page-link"><dl><dt>Pages :</dt><dd>',
-		'after'            => '</dd></dl></nav>',
-		'link_before'      => '<span class="page-numbers">',
-		'link_after'       => '</span>',
-		'echo'             => 1
-		);
-	wp_link_pages( $args ); ?>
-    </div><!-- [ /#post-<?php the_ID(); ?> ] -->
+		'before'      => '<nav class="page-link"><dl><dt>Pages :</dt><dd>',
+		'after'       => '</dd></dl></nav>',
+		'link_before' => '<span class="page-numbers">',
+		'link_after'  => '</span>',
+		'echo'        => 1,
+	);
+			wp_link_pages( $args );
+			?>
+			</div><!-- [ /#post-<?php the_ID(); ?> ] -->
 
 	<?php endif; ?>
 
-	<?php endwhile; ?>
+	<?php
+	endwhile;
+	};
+?>
 
 </div><!-- [ /.mainSection ] -->
 
--- sidebar-page.php
+++ sidebar-page.php
@@ -1,27 +1,28 @@
 <?php
-if ( is_active_sidebar( 'common-side-top-widget-area' ) )
-  dynamic_sidebar( 'common-side-top-widget-area' );
+if ( is_active_sidebar( 'common-side-top-widget-area' ) ) {
+	dynamic_sidebar( 'common-side-top-widget-area' );
+}
 
 // Display post type widget area
 $widdget_area_name = 'page-side-widget-area';
-if ( is_active_sidebar( $widdget_area_name ) ){
-  dynamic_sidebar( $widdget_area_name );
+if ( is_active_sidebar( $widdget_area_name ) ) {
+	dynamic_sidebar( $widdget_area_name );
 } else {
 
-	if($post->ancestors){
-		foreach($post->ancestors as $post_anc_id){
+	if ( $post->ancestors ) {
+		foreach ( $post->ancestors as $post_anc_id ) {
 			$post_id = $post_anc_id;
 		} // foreach($post->ancestors as $post_anc_id){
 	} else {
 		$post_id = $post->ID;
 	} // if($post->ancestors){
 
-	if ($post_id) {
-		$children = wp_list_pages("title_li=&child_of=".$post_id."&echo=0");
-		if ($children) { ?>
+	if ( $post_id ) {
+		$children = wp_list_pages( 'title_li=&child_of=' . $post_id . '&echo=0' );
+		if ( $children ) { ?>
 			<aside class="widget widget_child_page widget_link_list">
 			<nav class="localNav">
-			<h1 class="subSection-title"><?php echo get_the_title($post_id); ?></h1>
+			<h1 class="subSection-title"><?php echo get_the_title( $post_id ); ?></h1>
 			<ul>
 			<?php echo $children; ?>
 			</ul>
@@ -32,6 +33,7 @@ if ( is_active_sidebar( $widdget_area_name ) ){
 
 } // if ( is_active_sidebar( $widdget_area_name ) ){
 
-if ( is_active_sidebar( 'common-side-bottom-widget-area' ) )
-  dynamic_sidebar( 'common-side-bottom-widget-area' );
+if ( is_active_sidebar( 'common-side-bottom-widget-area' ) ) {
+	dynamic_sidebar( 'common-side-bottom-widget-area' );
+}
 ?>
