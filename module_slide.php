9ae4d13e3c102cff169bdf9df25dffbb97eb5707
kurudrive <kurudrive@gmail.com>
2019-07-09 11:49:51 +0900

    [ 仕様変更 ][ スライド ] デフォルトのスライドの表示制御用のフィルターフック部分最初の3行までの記述を変更しただけで他は全てその影響によるインデントずれのみ

--- module_slide.php
+++ module_slide.php
@@ -1,23 +1,22 @@
 <?php
-if ( ! apply_filters( 'lightning_default_slide_display', true ) ) {
-	return;
-}
-$lightning_theme_options = lightning_get_theme_options();
+if ( apply_filters( 'lightning_default_slide_display', true ) ) {
 
-// count top slide
-$top_slide_count_max = lightning_top_slide_count_max();
-$top_slide_count     = lightning_top_slide_count( $lightning_theme_options );
-$top_slide_count     = apply_filters( 'lightning_top_slide_count', $top_slide_count );
+	$lightning_theme_options = lightning_get_theme_options();
 
-if ( $top_slide_count ) : ?>
-<?php
-if ( empty( $lightning_theme_options['top_slide_time'] ) ) {
-	$interval = 4000;
-} else {
-	$interval = esc_attr( $lightning_theme_options['top_slide_time'] );
-}
-?>
-<div id="top__fullcarousel" data-interval="<?php echo $interval; ?>" class="carousel slide" data-ride="carousel">
+	// count top slide
+	$top_slide_count_max = lightning_top_slide_count_max();
+	$top_slide_count     = lightning_top_slide_count( $lightning_theme_options );
+	$top_slide_count     = apply_filters( 'lightning_top_slide_count', $top_slide_count );
+
+	if ( $top_slide_count ) : ?>
+	<?php
+	if ( empty( $lightning_theme_options['top_slide_time'] ) ) {
+		$interval = 4000;
+	} else {
+		$interval = esc_attr( $lightning_theme_options['top_slide_time'] );
+	}
+	?>
+	<div id="top__fullcarousel" data-interval="<?php echo $interval; ?>" class="carousel slide" data-ride="carousel">
 <div class="carousel-inner">
 
 	<?php if ( $top_slide_count >= 2 ) : ?>
@@ -32,32 +31,32 @@ if ( empty( $lightning_theme_options['top_slide_time'] ) ) {
 		</ol>
 	<?php endif; ?>
 
-	<?php
+		<?php
 		// Why end point is $top_slide_count_max that not $top_slide_count, image exist 1,2,5
-	for ( $i = 1; $i <= $top_slide_count_max; ) {
+		for ( $i = 1; $i <= $top_slide_count_max; ) {
 
 			$top_slide_url = '';
 
 			// If Alt exist
 			$top_slide_alt = '';
-		if ( ! empty( $lightning_theme_options[ 'top_slide_alt_' . $i ] ) ) {
-			$top_slide_alt = $lightning_theme_options[ 'top_slide_alt_' . $i ];
-		} elseif ( ! empty( $lightning_theme_options[ 'top_slide_title_' . $i ] ) ) {
-			$top_slide_alt = $lightning_theme_options[ 'top_slide_title_' . $i ];
-		} else {
-			$top_slide_alt = '';
-		}
+			if ( ! empty( $lightning_theme_options[ 'top_slide_alt_' . $i ] ) ) {
+				$top_slide_alt = $lightning_theme_options[ 'top_slide_alt_' . $i ];
+			} elseif ( ! empty( $lightning_theme_options[ 'top_slide_title_' . $i ] ) ) {
+				$top_slide_alt = $lightning_theme_options[ 'top_slide_title_' . $i ];
+			} else {
+				$top_slide_alt = '';
+			}
 
-		// Slide Display
-		if ( ! empty( $lightning_theme_options[ 'top_slide_image_' . $i ] ) ) {
-			$link_target = ( isset( $lightning_theme_options[ 'top_slide_link_blank_' . $i ] ) && $lightning_theme_options[ 'top_slide_link_blank_' . $i ] ) ? ' target="_blank"' : '';
-			?>
-			<div class="item item-<?php echo $i; ?>
+			// Slide Display
+			if ( ! empty( $lightning_theme_options[ 'top_slide_image_' . $i ] ) ) {
+				$link_target = ( isset( $lightning_theme_options[ 'top_slide_link_blank_' . $i ] ) && $lightning_theme_options[ 'top_slide_link_blank_' . $i ] ) ? ' target="_blank"' : '';
+				?>
+				<div class="item item-<?php echo $i; ?>
 												<?php
 												if ( $i == 1 ) {
 													echo ' active';}
-?>
-">
+	?>
+	">
 
 				<?php if ( lightning_is_slide_outer_link( $lightning_theme_options, $i ) ) : ?>
 						<a href="<?php echo esc_url( $lightning_theme_options[ 'top_slide_url_' . $i ] ); ?>"<?php echo $link_target; ?>>
@@ -70,21 +69,21 @@ if ( empty( $lightning_theme_options['top_slide_time'] ) ) {
 					?>
 						  <source media="(max-width: 767px)" srcset="<?php echo esc_attr( $lightning_theme_options[ 'top_slide_image_mobile_' . $i ] ); ?>">
 					<?php endif; ?>
-				  <img src="<?php echo esc_attr( $lightning_theme_options[ 'top_slide_image_' . $i ] ); ?>" alt="<?php echo esc_attr( $top_slide_alt ); ?>" class="slide-item-img">
-				</picture>
+					  <img src="<?php echo esc_attr( $lightning_theme_options[ 'top_slide_image_' . $i ] ); ?>" alt="<?php echo esc_attr( $top_slide_alt ); ?>" class="slide-item-img">
+					</picture>
 
-				<?php
-				/*-------------------------------------------*/
-				/*	slide-cover
-				/*-------------------------------------------*/
-				$cover_style = lightning_slide_cover_style( $lightning_theme_options, $i );
-				if ( $cover_style ) {
-					$cover_style = ( $cover_style ) ? ' style="' . esc_attr( $cover_style ) . '"' : '';
-					echo '<div class="slide-cover"' . $cover_style . '></div>';
-				}
-				?>
+					<?php
+					/*-------------------------------------------*/
+					/*	slide-cover
+					/*-------------------------------------------*/
+					$cover_style = lightning_slide_cover_style( $lightning_theme_options, $i );
+					if ( $cover_style ) {
+						$cover_style = ( $cover_style ) ? ' style="' . esc_attr( $cover_style ) . '"' : '';
+						echo '<div class="slide-cover"' . $cover_style . '></div>';
+					}
+					?>
 
-				<?php if ( lightning_is_slide_outer_link( $lightning_theme_options, $i ) ) : ?>
+					<?php if ( lightning_is_slide_outer_link( $lightning_theme_options, $i ) ) : ?>
 						</a>
 					<?php endif; ?>
 
@@ -138,10 +137,10 @@ if ( empty( $lightning_theme_options['top_slide_time'] ) ) {
 
 							// If Text Title exist
 					if ( $mini_content_args['title'] ) :
-					?>
+						?>
 					  <h3 class="slide-text-title"<?php echo $font_style; ?>>
-											<?php echo nl2br( wp_kses_post( $mini_content_args['title'] ) ); ?>
-									  </h3>
+										<?php echo nl2br( wp_kses_post( $mini_content_args['title'] ) ); ?>
+								  </h3>
 					<?php endif; ?>
 
 					<?php
@@ -178,21 +177,23 @@ if ( empty( $lightning_theme_options['top_slide_time'] ) ) {
 									<a class="btn btn-ghost" href="<?php echo esc_url( $mini_content_args['link_url'] ); ?>"<?php echo $mini_content_args['link_target']; ?>><?php echo wp_kses_post( $mini_content_args['btn_text'] ); ?></a>
 									<?php endif; ?>
 
-				</div><!-- .container -->
-						</div><!-- [ /.slide-text-set.mini-content  ] -->
-		  </div><!-- [ /.item ] -->
+					</div><!-- .container -->
+							</div><!-- [ /.slide-text-set.mini-content  ] -->
+			  </div><!-- [ /.item ] -->
 
-			<?php } // if ( $top_slide_image_src ) { ?>
-		<?php
-		$i++;
-	}
-	?>
-</div><!-- [ /.carousel-inner ] -->
+				<?php } // if ( $top_slide_image_src ) { ?>
+			<?php
+			$i++;
+		}
+		?>
+	</div><!-- [ /.carousel-inner ] -->
 
-<?php if ( $top_slide_count >= 2 ) : ?>
+	<?php if ( $top_slide_count >= 2 ) : ?>
 	<a class="left carousel-control" href="#top__fullcarousel" data-slide="prev"><i class="icon-prev fa fa-angle-left"></i></a>
 	<a class="right carousel-control" href="#top__fullcarousel" data-slide="next"><i class="icon-next fa fa-angle-right"></i></a>
-<?php endif; ?>
+	<?php endif; ?>
 
-</div><!-- [ /#top__fullcarousel ] -->
-<?php endif; ?>
+	</div><!-- [ /#top__fullcarousel ] -->
+	<?php endif;
+} // if ( apply_filters( 'lightning_default_slide_display', true ) ) {
+?>
