9492b67870f825b7b16e350485986c6038f43d19
kurudrive <kurudrive@gmail.com>
2019-08-12 23:02:34 +0900

    [ bs4 ] 動的出力カラー部分反映

--- design-skin/origin2/origin2.php
+++ design-skin/origin2/origin2.php
@@ -1,46 +1,52 @@
 <?php
 /*-------------------------------------------*/
-/*	Print head color style
+/*	Print head
 /*-------------------------------------------*/
-// !! Please rewrite the part of the [sample] to your design skin name.
-add_action( 'wp_head', 'lightning_origin2_print_css', 3 );
-function lightning_origin2_print_css() {
-	$options        = get_option( 'lightning_theme_options' );
-	$color_key      = ( isset( $options['color_key'] ) ) ? esc_html( $options['color_key'] ) : '#337ab7';
-	$color_key_dark = ( isset( $options['color_key_dark'] ) ) ? esc_html( $options['color_key_dark'] ) : '#2e6da4';
-	$fort_options   = get_option( 'lightning_fort_options' );
-	$color_taste    = ( isset( $fort_options['color_taste'] ) ) ? esc_html( $fort_options['color_taste'] ) : 'thick';
-	$dynamic_css    = '/* Fort */
-dt { border-left-color:' . $color_key . '; }
+add_action( 'wp_head', 'lightning_print_css_origin', 3 );
+function lightning_print_css_origin() {
+	$options = get_option( 'lightning_theme_options' );
+	if ( isset( $options['color_key'] ) && isset( $options['color_key_dark'] ) ) {
+		$color_key      = esc_html( $options['color_key'] );
+		$color_key_dark = esc_html( $options['color_key_dark'] );
+		$dynamic_css    = '
+a { color:' . $color_key_dark . ' ; }
+a:hover { color:' . $color_key . ' ; }
+.page-header { background-color:' . $color_key . '; }
+h2,
+.mainSection-title { border-top-color:' . $color_key . '; }
+h3:after,
+.subSection-title:after { border-bottom-color:' . $color_key . ';  }
+.media .media-body .media-heading a:hover { color:' . $color_key . ';  }
 ul.page-numbers li span.page-numbers.current { background-color:' . $color_key . '; }
+.pager li > a { border-color:' . $color_key . ';color:' . $color_key . ';}
+.pager li > a:hover { background-color:' . $color_key . ';color:#fff;}
+footer { border-top-color:' . $color_key . '; }
+dt { border-left-color:' . $color_key . '; }
+@media (min-width: 768px){
+  ul.gMenu > li > a:after { border-bottom-color: ' . $color_key . ' ; }
+} /* @media (min-width: 768px) */';
 
-.mainSection h2,
-.mainSection-title,
-.subSection .widget .subSection-title { border-top-color:' . $color_key . '; }
+		if ( ! empty( $options['color_header_bg'] ) ) {
+			$color_header_bg = esc_html( $options['color_header_bg'] );
+			if ( lightning_check_color_mode( $color_header_bg ) == 'dark' ) {
+				// Dark Color ///////////////////
+				$dynamic_css .= '
+			@media (min-width: 768px){
+				ul.gMenu > li > a:after { border-bottom-color: rgba(255,255,255,0.9 );}
+			}';
+			} else {
+				// Light Color ///////////////////
+			}
+		}// if ( ! empty( $options['color_header_bg'] ) ) {
 
-.siteFooter {  border-top-color:' . $color_key . '; }
-
-@media (min-width: 992px){
-.gMenu_outer { border-top-color:' . $color_key . '; }
-ul.gMenu > li:hover > a .gMenu_description,
-ul.gMenu > li.current-post-ancestor > a .gMenu_description,
-ul.gMenu > li.current-menu-item > a .gMenu_description,
-ul.gMenu > li.current-menu-parent > a .gMenu_description,
-ul.gMenu > li.current-menu-ancestor > a .gMenu_description,
-ul.gMenu > li.current_page_parent > a .gMenu_description,
-ul.gMenu > li.current_page_ancestor > a .gMenu_description { color: ' . $color_key . '; }
-.gMenu_outer { border-top-color:' . $color_key_dark . '; }
-} /* @media (min-width: 768px){ */
-
-.btn-default { border-color:#e5e5e5; color:#535353; }
-';
-	// delete before after space
-	$dynamic_css = trim( $dynamic_css );
-	// convert tab and br to space
-	$dynamic_css = preg_replace( '/[\n\r\t]/', '', $dynamic_css );
-	// Change multiple spaces to single space
-	$dynamic_css = preg_replace( '/\s(?=\s)/', '', $dynamic_css );
-	wp_add_inline_style( 'lightning-design-style', $dynamic_css );
+		// delete before after space
+		$dynamic_css = trim( $dynamic_css );
+		// convert tab and br to space
+		$dynamic_css = preg_replace( '/[\n\r\t]/', '', $dynamic_css );
+		// Change multiple spaces to single space
+		$dynamic_css = preg_replace( '/\s(?=\s)/', '', $dynamic_css );
+		wp_add_inline_style( 'lightning-design-style', $dynamic_css );
+	} // if ( isset($options['color_key'] && isset($options['color_key_dark'] ) {
 }
 
 /*-------------------------------------------*/
