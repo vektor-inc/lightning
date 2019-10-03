be8ecef414748a7b29183da0254926ef58f4b195
kurudrive <kurudrive@gmail.com>
2019-10-04 03:56:28 +0900

    [ テンプレート階層変更 ] module_loop_***.php リダイレクト処理 カスタム投稿タイプ対応

--- inc/template-redirect.php
+++ inc/template-redirect.php
@@ -11,14 +11,23 @@
  * @param  string $slug [description]
  * @return [type]       [description]
  */
-function lightning_change_module_loop_post( $slug ) {
 
-	$templates[] = "{$slug}.php";
+add_action( 'after_setup_theme', 'lightning_redirect_template_module_loop' );
+function lightning_redirect_template_module_loop() {
+	$postTypes = get_post_types( array( 'public' => true ) );
+	foreach ( $postTypes as $postType ) {
+		add_action(
+			'get_template_part_module_loop_' . $postType, function( $slug ) {
 
-	// If old name file is don't exist that load new name file.
-	if ( ! locate_template( $templates, false, false ) ) {
-		$templates[] = 'template-parts/loop-post.php';
-		locate_template( $templates, true, false );
-	}
+				$templates[] = "{$slug}.php";
+
+				// If old name file is don't exist that load new name file.
+				if ( ! locate_template( $templates, false, false ) ) {
+					get_template_part( 'template-parts/loop', get_post_type() );
+					// $templates[] = 'template-parts/loop-post.php';
+					// locate_template( $templates, true, false );
+				}
+			}
+		);
+	} // foreach ( $postTypes as $postType ) {
 }
-add_action( 'get_template_part_module_loop_post', 'lightning_change_module_loop_post' );
