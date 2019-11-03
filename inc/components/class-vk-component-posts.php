cd515d7768f1121e8c508495567f27d96cd78380
kurudrive <kurudrive@gmail.com>
2019-11-04 00:52:36 +0900

    [ components/posts ] ループメソッド追加

--- inc/components/class-vk-component-posts.php
+++ inc/components/class-vk-component-posts.php
@@ -58,6 +58,44 @@ if ( ! class_exists( 'VK_Component_Posts' ) ) {
 			 echo wp_kses_post( self::get_view( $post, $options ) );
 		}
 
+		static public function get_loop( $wp_query, $options, $options_loop = array() ) {
+
+			$options_loop_dafault = array(
+				'class_loop_outer' => '',
+			);
+			$options_loop         = wp_parse_args( $options_loop, $options_loop_dafault );
+
+			$loop = '';
+			if ( $wp_query->have_posts() ) :
+
+				$outer_class = '';
+				if ( $options_loop['class_loop_outer'] ) {
+					$outer_class = ' ' . $options_loop['class_loop_outer'];
+				}
+
+				$loop .= '<div class="vk_posts' . $outer_class . '">';
+
+				while ( $wp_query->have_posts() ) {
+					$wp_query->the_post();
+					$term_args          = array(
+						'class' => 'card_singleTermLabel',
+					);
+					$options['overlay'] = Vk_term_color::get_single_term_with_color( false, $term_args );
+					global $post;
+					$loop .= VK_Component_Posts::get_view( $post, $options );
+				} // while ( have_posts() ) {
+				endif;
+
+				$loop .= '</div>';
+
+			wp_reset_query();
+			wp_reset_postdata();
+			return $loop;
+		}
+		static public function the_loop( $wp_query, $options, $options_loop = array() ) {
+			echo self::get_loop( $wp_query, $options, $options_loop );
+		}
+
 		/*-------------------------------------------*/
 		/* Common Parts
 		/*-------------------------------------------*/
