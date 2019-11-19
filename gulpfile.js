48b346479034b075a3e160d5680a6ddca5c9817c
kurudrive <kurudrive@gmail.com>
2019-11-20 04:37:19 +0900

    [ gulp ] watch修正

--- gulpfile.js
+++ gulpfile.js
@@ -165,7 +165,7 @@ gulp.task('js_build', function() {
 // Watch
 gulp.task('watch', function() {
   gulp.watch(['./inc/vk-components/*.php'], ['components_copy']);
-  gulp.watch(['./inc/vk-components/**/*.scss'], ['sass_skin2']);
+  gulp.watch(['./inc/vk-components/**/*.scss'], ['sass_common']);
   gulp.watch(['./assets/js/**','./inc/vk-mobile-nav/package/js/**'], ['js_build']);
   gulp.watch(['./assets/_scss/**','./inc/woocommerce/_scss/**'], ['sass_common']);
   gulp.watch(['./assets/_scss/**','./inc/woocommerce/_scss/**'], ['sass_woo']);
