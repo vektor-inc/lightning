bc1a3abbf54c26b2086c5f7545fa275ed414d514
kurudrive <kurudrive@gmail.com>
2019-12-12 17:22:30 +0900

    phpcs

--- single.php
+++ single.php
@@ -41,69 +41,72 @@ else :
 		?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="<?php lightning_the_class_name( 'entry-header' ); ?>">
-		<?php get_template_part( 'module_loop_post_meta' ); ?>
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+			<?php get_template_part( 'template-parts/post/meta.php' ); ?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
 		</header>
 
 		<?php do_action( 'lightning_entry_body_before' ); ?>
+
 		<div class="<?php lightning_the_class_name( 'entry-body' ); ?>">
 		<?php the_content(); ?>
 		</div>
+
 		<?php do_action( 'lightning_entry_body_after' ); ?>
 
 		<div class="<?php lightning_the_class_name( 'entry-footer' ); ?>">
-		<?php
-		$args = array(
-			'before'      => '<nav class="page-link"><dl><dt>Pages :</dt><dd>',
-			'after'       => '</dd></dl></nav>',
-			'link_before' => '<span class="page-numbers">',
-			'link_after'  => '</span>',
-			'echo'        => 1,
-		);
+
+			<?php
+			$args = array(
+				'before'      => '<nav class="page-link"><dl><dt>Pages :</dt><dd>',
+				'after'       => '</dd></dl></nav>',
+				'link_before' => '<span class="page-numbers">',
+				'link_after'  => '</span>',
+				'echo'        => 1,
+			);
 			wp_link_pages( $args );
 			?>
 
-				<?php
-				/*-------------------------------------------*/
-				/*  Category and tax data
-				/*-------------------------------------------*/
-				$args          = array(
-					'template'      => __( '<dl><dt>%s</dt><dd>%l</dd></dl>', 'lightning' ),
-					'term_template' => '<a href="%1$s">%2$s</a>',
-				);
-				$taxonomies    = get_the_taxonomies( $post->ID, $args );
-				$taxnomiesHtml = '';
-				if ( $taxonomies ) {
-					foreach ( $taxonomies as $key => $value ) {
-						if ( $key != 'post_tag' ) {
-							$taxnomiesHtml .= '<div class="entry-meta-dataList">' . $value . '</div>';
-						}
-					} // foreach
-				} // if ($taxonomies)
-				$taxnomiesHtml = apply_filters( 'lightning_taxnomiesHtml', $taxnomiesHtml );
-				echo $taxnomiesHtml;
-			?>
-
 			<?php
+			/*-------------------------------------------*/
+			/*  Category and tax data
+			/*-------------------------------------------*/
+			$args          = array(
+				'template'      => __( '<dl><dt>%s</dt><dd>%l</dd></dl>', 'lightning' ),
+				'term_template' => '<a href="%1$s">%2$s</a>',
+			);
+			$taxonomies    = get_the_taxonomies( $post->ID, $args );
+			$taxnomiesHtml = '';
+			if ( $taxonomies ) {
+				foreach ( $taxonomies as $key => $value ) {
+					if ( $key != 'post_tag' ) {
+						$taxnomiesHtml .= '<div class="entry-meta-dataList">' . $value . '</div>';
+					}
+				} // foreach
+			} // if ($taxonomies)
+			$taxnomiesHtml = apply_filters( 'lightning_taxnomiesHtml', $taxnomiesHtml );
+			echo $taxnomiesHtml;
+
+			// tag list
 			$tags_list = get_the_tag_list();
-			if ( $tags_list ) :
-			?>
+			if ( $tags_list ) {	?>
 			<div class="entry-meta-dataList entry-tag">
 			<dl>
 			<dt><?php _e( 'Tags', 'lightning' ); ?></dt>
-	<dd class="tagcloud"><?php echo $tags_list; ?></dd>
-	</dl>
-	</div><!-- [ /.entry-tag ] -->
-	<?php endif; ?>
+			<dd class="tagcloud"><?php echo $tags_list; ?></dd>
+			</dl>
+			</div><!-- [ /.entry-tag ] -->
+			<?php } // if ( $tags_list ) { ?>
+
 		</div><!-- [ /.entry-footer ] -->
 
 		<?php comments_template( '', true ); ?>
+
 	</article>
 
 	<?php
 	endwhile;
-	endif;
-	endif;
+	endif; // if ( have_posts() ) :
+	endif; // if ( apply_filters( 'is_lightning_extend_single', false ) ) :
 ?>
 <?php get_template_part( 'template-parts/post/next-prev' ); ?>
 <?php do_action( 'lightning_mainSection_append' ); ?>
