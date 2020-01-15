d151386c5b22bd1afb41379e95b539b2e6b0993c
kurudrive <kurudrive@gmail.com>
2020-01-15 14:28:21 +0900

    phpcs

--- single.php
+++ single.php
@@ -3,8 +3,9 @@
 <?php
 // Dealing with old files.
 // Actually, it's ok to only use get_template_part().
-/*-------------------------------------------*/
-/* Page Header
+
+/*
+ Page Header
 /*-------------------------------------------*/
 $old_file_name[] = 'module_pageTit.php';
 if ( locate_template( $old_file_name, false, false ) ) {
@@ -12,8 +13,9 @@ if ( locate_template( $old_file_name, false, false ) ) {
 } else {
 	get_template_part( 'template-parts/page-header' );
 }
-/*-------------------------------------------*/
-/* BreadCrumb
+
+/*
+ BreadCrumb
 /*-------------------------------------------*/
 $old_file_name[] = 'module_panList.php';
 if ( locate_template( $old_file_name, false, false ) ) {
@@ -38,70 +40,70 @@ else :
 	if ( have_posts() ) :
 		while ( have_posts() ) :
 			the_post();
-		?>
+			?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="<?php lightning_the_class_name( 'entry-header' ); ?>">
 			<?php get_template_part( 'template-parts/post/meta' ); ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 		</header>
 
-		<?php do_action( 'lightning_entry_body_before' ); ?>
+			<?php do_action( 'lightning_entry_body_before' ); ?>
 
-		<div class="<?php lightning_the_class_name( 'entry-body' ); ?>">
-		<?php the_content(); ?>
-		</div>
+			<div class="<?php lightning_the_class_name( 'entry-body' ); ?>">
+				<?php the_content(); ?>
+			</div>
 
-		<?php do_action( 'lightning_entry_body_after' ); ?>
+			<?php do_action( 'lightning_entry_body_after' ); ?>
 
-		<div class="<?php lightning_the_class_name( 'entry-footer' ); ?>">
+			<div class="<?php lightning_the_class_name( 'entry-footer' ); ?>">
 
-			<?php
-			$args = array(
-				'before'      => '<nav class="page-link"><dl><dt>Pages :</dt><dd>',
-				'after'       => '</dd></dl></nav>',
-				'link_before' => '<span class="page-numbers">',
-				'link_after'  => '</span>',
-				'echo'        => 1,
-			);
-			wp_link_pages( $args );
-			?>
+				<?php
+				$args = array(
+					'before'      => '<nav class="page-link"><dl><dt>Pages :</dt><dd>',
+					'after'       => '</dd></dl></nav>',
+					'link_before' => '<span class="page-numbers">',
+					'link_after'  => '</span>',
+					'echo'        => 1,
+				);
+				wp_link_pages( $args );
+				?>
 
-			<?php
-			/*-------------------------------------------*/
-			/*  Category and tax data
-			/*-------------------------------------------*/
-			$args          = array(
-				'template'      => __( '<dl><dt>%s</dt><dd>%l</dd></dl>', 'lightning' ),
-				'term_template' => '<a href="%1$s">%2$s</a>',
-			);
-			$taxonomies    = get_the_taxonomies( $post->ID, $args );
-			$taxnomiesHtml = '';
-			if ( $taxonomies ) {
-				foreach ( $taxonomies as $key => $value ) {
-					if ( $key != 'post_tag' ) {
-						$taxnomiesHtml .= '<div class="entry-meta-dataList">' . $value . '</div>';
-					}
-				} // foreach
-			} // if ($taxonomies)
-			$taxnomiesHtml = apply_filters( 'lightning_taxnomiesHtml', $taxnomiesHtml );
-			echo $taxnomiesHtml;
+				<?php
+				/*
+				Category and tax data
+				/*-------------------------------------------*/
+				$args          = array(
+					'template'      => __( '<dl><dt>%s</dt><dd>%l</dd></dl>', 'lightning' ),
+					'term_template' => '<a href="%1$s">%2$s</a>',
+				);
+				$taxonomies    = get_the_taxonomies( $post->ID, $args );
+				$taxnomiesHtml = '';
+				if ( $taxonomies ) {
+					foreach ( $taxonomies as $key => $value ) {
+						if ( $key != 'post_tag' ) {
+							$taxnomiesHtml .= '<div class="entry-meta-dataList">' . $value . '</div>';
+						}
+					} // foreach
+				} // if ($taxonomies)
+				$taxnomiesHtml = apply_filters( 'lightning_taxnomiesHtml', $taxnomiesHtml );
+				echo $taxnomiesHtml;
 
-			// tag list
-			$tags_list = get_the_tag_list();
-			if ( $tags_list ) {	?>
-			<div class="entry-meta-dataList entry-tag">
-			<dl>
-			<dt><?php _e( 'Tags', 'lightning' ); ?></dt>
-			<dd class="tagcloud"><?php echo $tags_list; ?></dd>
-			</dl>
-			</div><!-- [ /.entry-tag ] -->
-			<?php } // if ( $tags_list ) { ?>
+				// tag list
+				$tags_list = get_the_tag_list();
+				if ( $tags_list ) { ?>
+				<div class="entry-meta-dataList entry-tag">
+					<dl>
+					<dt><?php _e( 'Tags', 'lightning' ); ?></dt>
+					<dd class="tagcloud"><?php echo $tags_list; ?></dd>
+					</dl>
+				</div><!-- [ /.entry-tag ] -->
+				<?php } // if ( $tags_list ) { ?>
 
-		</div><!-- [ /.entry-footer ] -->
+			</div><!-- [ /.entry-footer ] -->
 
-		<?php do_action( 'lightning_comment_before' ); ?>
-		<?php comments_template( '', true ); ?>
-		<?php do_action( 'lightning_comment_after' ); ?>
+			<?php do_action( 'lightning_comment_before' ); ?>
+			<?php comments_template( '', true ); ?>
+			<?php do_action( 'lightning_comment_after' ); ?>
 
 	</article>
 
