a4d365bc846e74c08cdbfecafcf9a47c75573ed0
kurudrive <kurudrive@gmail.com>
2020-02-09 14:51:45 +0900

    [ 高速化対策 ] 共通CSSとテーマstyleの読み込みをフッターに変更 UI 実装

--- inc/enqueue-point-controller/enqueue-point-controller.php
+++ inc/enqueue-point-controller/enqueue-point-controller.php
@@ -1,9 +1,61 @@
 <?php
 
-add_action( 'after_setup_theme', 'lightning_change_enqueue_point_run_filter',5 );
+
+/*
+/*	customize_register
+/*-------------------------------------------*/
+add_action( 'customize_register', 'lightning_customize_register_enqueue_point' );
+function lightning_customize_register_enqueue_point( $wp_customize ) {
+
+	$wp_customize->add_setting(
+		'enqueue_point_title', array(
+			'sanitize_callback' => 'sanitize_text_field',
+		)
+	);
+	$wp_customize->add_control(
+		new Custom_Html_Control(
+			$wp_customize, 'enqueue_point_title', array(
+				'label'            => __( 'CSS and Javascript load point ', 'lightning' ),
+				'section'          => 'lightning_function',
+				'type'             => 'text',
+				'custom_title_sub' => '',
+			)
+		)
+	);
+
+	$wp_customize->add_setting(
+		"lightning_theme_options[enqueue_point_footer]", array(
+			'default'           => false,
+			'type'              => 'option',
+			'capability'        => 'edit_theme_options',
+			'sanitize_callback' => 'lightning_sanitize_checkbox',
+		)
+	);
+	$wp_customize->add_control(
+		"lightning_theme_options[enqueue_point_footer]", array(
+			'label'       => __( 'Move to footer', 'lightning' ),
+			'section'     => 'lightning_function',
+			'settings'    => "lightning_theme_options[enqueue_point_footer]",
+			'type'        => 'checkbox',
+			'description' => 'If you enabled this checkbox that page loading speed become little faster.',
+		)
+	);
+
+}
+
+
+/**
+ * Change load point
+ */
+
+ add_action( 'after_setup_theme', 'lightning_change_enqueue_point_run_filter',5 );
 function lightning_change_enqueue_point_run_filter(){
 
-	// font awesome
+	$options = get_option( 'lightning_theme_options' );
+
+	if ( ! empty( $options['enqueue_point_footer'] ) ){
+
+	// Theme common and theme-style
 	add_filter( 'lightning_enqueue_point_common_and_theme_css', 'lightning_change_enqueue_point_to_footer');
 
 	// font awesome
@@ -14,6 +66,8 @@ function lightning_change_enqueue_point_run_filter(){
 
 	// vk blocks css
 	add_filter( 'vkblocks_enqueue_point', 'lightning_change_enqueue_point_to_footer' );
+
+	}
 
 }
 
