45ca70526d543a56a1c9a077d8af5cf82edb8f11
kurudrive <kurudrive@gmail.com>
2020-02-15 01:03:50 +0900

    phpcs

--- inc/layout-controller/functions-layout-controll.php
+++ inc/layout-controller/functions-layout-controll.php
@@ -155,11 +155,11 @@ function lightning_is_page_header_and_breadcrumb() {
 	return apply_filters( 'lightning_is_page_header_and_breadcrumb', $return );
 }
 
-function lightning_is_siteContent_padding_off(){
-	if ( is_singular() ){
+function lightning_is_siteContent_padding_off() {
+	if ( is_singular() ) {
 		global $post;
 		$cf = $post->_lightning_design_setting;
-		if ( !empty( $cf['siteContent_padding'] ) ){
+		if ( ! empty( $cf['siteContent_padding'] ) ) {
 			return true;
 		}
 	}
