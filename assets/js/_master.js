60c531cceee1e68fbd77fd41ec8c36742d33d34f
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-03-18 11:21:02 +0900

    一時保存

--- assets/js/_master.js
+++ assets/js/_master.js
@@ -7,7 +7,7 @@
     // addClass_dropdown();
   });
 
-  document.addEventListener('load', iframe_responsive);
+  document.addEventListener('DOMContentLoaded', iframe_responsive);
   document.addEventListener('resize', () => {
     iframe_responsive();
     let wrap_width =  document.defaultView.getComputedStyle(document.body, null).width;
@@ -32,20 +32,31 @@
   /*----------------------------------------------------------*/
   /*	gMenu control
   /*----------------------------------------------------------*/
+  let toggle = (selector, remover, adder) => {
+    Array.prototype.forEach.call(
+      document.querySelectorAll(selector),
+      (elem) => {
+        elem.classList.remove(remover)
+        elem.classList.add(adder)
+      }
+    );
+  }
   function run_menu_control() {
     let menu_control = () => {
-      if (!jQuery('.menuBtn').hasClass('menuOpen')) {
-        jQuery('body').removeClass('headerMenuClose').addClass('headerMenuOpen'); // 今後廃止
-        jQuery('body').removeClass('header-menu-close').addClass('header-menu-open');
-        jQuery('.menuBtn').removeClass('menuClose').addClass('menuOpen');
-        jQuery('#gMenu_outer').removeClass('itemClose').addClass('itemOpen');
-        jQuery('#menuBtn i').removeClass('fa-bars').addClass('fa-times');
+      if (!$('.menuBtn').hasClass('menuOpen')) {
+        document.body.classList.remove('headerMenuClose') // 今後廃止
+        document.body.classList.add('headerMenuOpen') // 今後廃止
+        document.body.classList.remove('header-menu-close')
+        document.body.classList.add('header-menu-open')
+        toggle('.menuBtn', 'menuClose', 'menuOpen')
+        toggle('#gMenu_outer', 'itemClose', 'itemOpen')
+        toggle('#menuBtn i', 'fa-bars', 'fa-times')
       } else {
-        jQuery('body').removeClass('headerMenuOpen'); // 今後廃止
-        jQuery('body').removeClass('header-menu-open');
-        jQuery('.menuBtn').removeClass('menuOpen').addClass('menuClose');
-        jQuery('#gMenu_outer').removeClass('itemOpen').addClass('itemClose');
-        jQuery('#menuBtn i').removeClass('fa-times').addClass('fa-bars');
+        document.body.classList.remove('headerMenuOpen') // 今後廃止
+        document.body.classList.remove('header-menu-open')
+        toggle('.menuBtn', 'menuOpen', 'menuClose')
+        toggle('#gMenu_outer', 'itemkOpen', 'itemClose')
+        toggle('#menuBtn i', 'fa-times', 'fa-bars')
       }
     }
     Array.prototype.forEach.call(document.getElementsByClassName('menuBtn'), (elem) => {
@@ -54,67 +65,75 @@
   }
 
   function menu_close() {
-    jQuery('body').removeClass('headerMenuOpen');
-    jQuery('.menuBtn').removeClass('menuOpen').addClass('menuClose');
-    jQuery('#gMenu_outer').removeClass('itemOpen').addClass('itemClose');
-    jQuery('#menuBtn i').removeClass('fa-times').addClass('fa-bars');
+    $('body').removeClass('headerMenuOpen');
+    $('.menuBtn').removeClass('menuOpen').addClass('menuClose');
+    $('#gMenu_outer').removeClass('itemOpen').addClass('itemClose');
+    $('#menuBtn i').removeClass('fa-times').addClass('fa-bars');
   }
 
   /*----------------------------------------------------------*/
   /*	Top slide control
   /*----------------------------------------------------------*/
+  let addClass = (selector, cls) => {
+    Array.prototype.forEach.call(
+      document.querySelectorAll(selector),
+      (elem) => elem.classList.add(cls)
+    );
+  }
   // add active class to first item
-  jQuery(document).ready(function() {
-    jQuery('#top__fullcarousel .carousel-indicators li:first-child').addClass("active");
-    jQuery('#top__fullcarousel .item:first-child').addClass("active");
-  });
+  document.addEventListener('DOMContentLoaded', () => {
+    addClass('#top__fullcarousel .carousel-indicators li:first-child', "active")
+    addClass('#top__fullcarousel .item:first-child', "active")
+  })
 
   /*-------------------------------------------*/
   /*	iframeのレスポンシブ対応
   /*-------------------------------------------*/
   function iframe_responsive() {
-    jQuery('iframe').each(function(i) {
-      var iframeUrl = jQuery(this).attr("src");
-      if (!iframeUrl) {
-        return;
+    new Array(document.getElementsByTagName('iframe'))
+      .each((i) => {
+        let iframeUrl = i.getAttribute('class')
+        if(!iframeUrl){return}
+        // iframeのURLの中に youtube か map が存在する位置を検索する
+        // 見つからなかった場合には -1 が返される
+        if (
+          (iframeUrl.indexOf("youtube") != -1) ||
+          (iframeUrl.indexOf("vimeo") != -1) ||
+          (iframeUrl.indexOf("maps") != -1)
+        ) {
+          var iframeWidth = i.getAttribute("width");
+          var iframeHeight = i.getAttribute("height");
+          var iframeRate = iframeHeight / iframeWidth;
+          var nowIframeWidth =  document.defaultView.getComputedStyle(i, null).width
+          var newIframeHeight = nowIframeWidth * iframeRate;
+          $(this).css({
+            "max-width": "100%",
+            "height": newIframeHeight
+          });
+        }
       }
-      // iframeのURLの中に youtube か map が存在する位置を検索する
-      // 見つからなかった場合には -1 が返される
-      if (
-        (iframeUrl.indexOf("youtube") != -1) ||
-        (iframeUrl.indexOf("vimeo") != -1) ||
-        (iframeUrl.indexOf("maps") != -1)
-      ) {
-        var iframeWidth = jQuery(this).attr("width");
-        var iframeHeight = jQuery(this).attr("height");
-        var iframeRate = iframeHeight / iframeWidth;
-        var nowIframeWidth = jQuery(this).width();
-        var newIframeHeight = nowIframeWidth * iframeRate;
-        jQuery(this).css({
-          "max-width": "100%",
-          "height": newIframeHeight
-        });
-      }
-    });
+    )
   }
 
   /*----------------------------------------------------------*/
   /*	add bootstrap class
   /*----------------------------------------------------------*/
-  document.addEventListener('load', () => {
-    jQuery('textarea').addClass("form-control");
-    jQuery('select').addClass("form-control");
-    jQuery('input[type=text]').addClass("form-control");
-    jQuery('input[type=number]').addClass("form-control");
-    jQuery('input[type=search]').addClass("form-control");
-    jQuery('input[type=password]').addClass("form-control");
-    jQuery('input[type=email]').addClass("form-control");
-    jQuery('input[type=tel]').addClass("form-control");
-    jQuery('input[type=submit]').addClass("btn btn-primary");
-    jQuery('#respond p').each(function(i) {
-      jQuery(this).children('input').appendTo(jQuery(this));
+  document.addEventListener('DOMContentLoaded', () => {
+    addClass('textarea', 'form-control')
+    addClass('select', 'form-control')
+    addClass('input[type=text]', 'form-control')
+    addClass('input[type=number]', 'form-control')
+    addClass('input[type=search]', 'form-control')
+    addClass('input[type=password]', 'form-control')
+    addClass('input[type=email]', 'form-control')
+    addClass('input[type=tel]', 'form-control')
+    addClass('input[type=submit]', 'btn')
+    addClass('input[type=submit]', 'btn-primary')
+    $('#respond p').each(function(i) {
+      $(this).children('input').appendTo($(this));
     });
-    jQuery('form#searchform').addClass('form-inline');
-    jQuery('form#searchform input[type=text]').addClass('form-group');
+    addClass('form#searchform', 'form-inline')
+    addClass('form#searchform input[type=text]', 'form-group')
   }, false)
-})(window,document,jQuery)
+})(window, document, jQuery)
+
