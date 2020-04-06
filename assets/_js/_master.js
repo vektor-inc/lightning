a6eb085f7859d0d7968e62e8ebfff87cfb1c33b2
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-04-06 13:05:44 +0900

    js修正

--- assets/_js/_master.js
+++ assets/_js/_master.js
@@ -13,32 +13,52 @@
         }else{
             document.body.classList.remove('scrolled')
         }
-        if(lightningOpt.header_scrool && window.pageYOffset > 160){
-            document.body.classList.add('header_scrolled')
-        }else{
-            document.body.classList.remove('header_scrolled')
-        }
     }
     window.addEventListener('scroll', bodyClass)
     window.addEventListener('DOMContentLoaded', bodyClass)
 
-    let remove_header = (e) => {
-        document.body.classList.remove('header_scrolled');
-    }
-
-    window.addEventListener('DOMContentLoaded', () => {
-        Array.prototype.forEach.call(
-            document.getElementsByTagName('a'),
-            (elem) => {
-                let href = elem.getAttribute('href')
-                if(!href || href.indexOf('#') != 0) return;
-                if (['tab', 'button'].indexOf(elem.getAttribute('role')) > 0) return;
-                if (elem.getAttribute('data-toggle')) return;
-                if (elem.getAttribute('carousel-control')) return;
-                elem.addEventListener('click', remove_header)
+    if(lightningOpt.header_scrool){
+        let body_class_timer = false;
+        let body_class_lock = false;
+        let header_scrool_func = ()=>{
+            if(!body_class_lock && window.pageYOffset > 160){
+                document.body.classList.add('header_scrolled')
+            }else{
+                document.body.classList.remove('header_scrolled')
             }
-        )
-    });
+        }
+
+        let remove_header = (e) => {
+            e.preventDefault()
+            document.body.classList.remove('header_scrolled')
+            window.removeEventListener('scroll', header_scrool_func)
+            if (body_class_timer !== false) {
+                clearTimeout(body_class_timer)
+            }
+            body_class_lock = true
+            body_class_timer = setTimeout(()=>{
+                window.addEventListener('scroll', header_scrool_func, true)
+                body_class_lock = false
+            }, 3000);
+        }
+
+        window.addEventListener('DOMContentLoaded', () => {
+            Array.prototype.forEach.call(
+                document.getElementsByTagName('a'),
+                (elem) => {
+                    let href = elem.getAttribute('href')
+                    if(!href || href.indexOf('#') != 0) return;
+                    if (['tab', 'button'].indexOf(elem.getAttribute('role')) > 0) return;
+                    if (elem.getAttribute('data-toggle')) return;
+                    if (elem.getAttribute('carousel-control')) return;
+                    elem.addEventListener('click', remove_header)
+                }
+            )
+        });
+
+        window.addEventListener('scroll', header_scrool_func, true)
+        window.addEventListener('DOMContentLoaded', header_scrool_func, false)
+    }
 
     /*----------------------------------------------------------*/
     /* gMenu control
