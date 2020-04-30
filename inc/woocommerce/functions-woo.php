c110ea3a0df902377325298a1e89f218bb0c1e2c
kurudrive <kurudrive@gmail.com>
2020-04-30 16:32:18 +0900

    [ 仕様変更 ] WooCommerce の cssが編集画面にも反映されるように調整

--- inc/woocommerce/functions-woo.php
+++ inc/woocommerce/functions-woo.php
@@ -11,11 +11,14 @@ add_action( 'after_setup_theme', 'lightning_add_woocommerce_support' );
   Load CSS
 /*-------------------------------------------*/
 function lightning_woo_css() {
-	$editor_style_path = get_template_directory_uri() . '/inc/woocommerce/css/woo.css';
-	wp_enqueue_style( 'lightning-woo-style', $editor_style_path, array( 'lightning-common-style' ), LIGHTNING_THEME_VERSION );
-	add_editor_style( $editor_style_path );
+	wp_enqueue_style( 'lightning-woo-style', get_template_directory_uri() . '/inc/woocommerce/css/woo.css', array( 'lightning-common-style' ), LIGHTNING_THEME_VERSION );
 }
 add_action( 'wp_enqueue_scripts', 'lightning_woo_css' );
+
+function lightning_add_woocommerce_css_to_editor(){
+	add_editor_style( '/inc/woocommerce/css/woo.css' );
+}
+add_action( 'after_setup_theme', 'lightning_add_woocommerce_css_to_editor' );
 
 /*
   WidgetArea initiate
@@ -50,40 +53,3 @@ function lightning_widgets_init_product() {
 
 }
 add_action( 'widgets_init', 'lightning_widgets_init_product' );
-
-/*
-  Print Dynamic css
-/*-------------------------------------------*/
-$options = get_option( 'lightning_theme_options' );
-if ( ! empty( $options['enqueue_point_footer'] ) ) {
-	add_action( 'wp_footer', 'lightning_print_css_woo', 20 );
-} else {
-	add_action( 'wp_head', 'lightning_print_css_woo', 20 );
-}
-function lightning_print_css_woo() {
-	$options     = get_option( 'lightning_theme_options' );
-	$dynamic_css = '';
-
-	if ( isset( $options['color_key'] ) && isset( $options['color_key_dark'] ) ) {
-		$color_key      = ( ! empty( $options['color_key'] ) ) ? esc_html( $options['color_key'] ) : '#337ab7';
-		$color_key_dark = ( ! empty( $options['color_key_dark'] ) ) ? esc_html( $options['color_key_dark'] ) : '#2e6da4';
-		$dynamic_css   .= '/* ltg Woo custom */ 
-		.woocommerce ul.product_list_widget li a:hover img { border-color:' . $color_key . '; }
-		';
-	} // if ( isset($options['color_key'] && isset($options['color_key_dark'] ) {
-
-	if ( $dynamic_css ) {
-		// delete br
-		$dynamic_css = str_replace( PHP_EOL, '', $dynamic_css );
-		// delete tab
-		$dynamic_css = preg_replace( '/[\n\r\t]/', '', $dynamic_css );
-		// multi space convert to single space
-		$dynamic_css = preg_replace( '/\s(?=\s)/', '', $dynamic_css );
-
-		// wp_add_inline_style() is not stable on change enquepoint system.
-		echo '<style id="lightning-woo-style-custom" type="text/css">' . $dynamic_css . '</style>';
-		// wp_add_inline_style( 'lightning-common-style', $dynamic_css );
-
-	}
-
-}
