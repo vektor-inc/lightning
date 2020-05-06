64d441575b8f7d9d84a1fe3d656606aa4205c6be
kurudrive <kurudrive@gmail.com>
2020-05-07 03:12:22 +0900

    phpcs

--- single.php
+++ single.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
 <?php
-if ( lightning_is_page_header_and_breadcrumb() ){
+if ( lightning_is_page_header_and_breadcrumb() ) {
 
 	// Dealing with old files.
 	// Actually, it's ok to only use get_template_part().
-	/*-------------------------------------------*/
-	/* Page Header
+	/*
+	 Page Header
 	/*-------------------------------------------*/
 	$old_file_name[] = 'module_pageTit.php';
 	if ( locate_template( $old_file_name, false, false ) ) {
@@ -14,8 +14,9 @@ if ( lightning_is_page_header_and_breadcrumb() ){
 	} else {
 		get_template_part( 'template-parts/page-header' );
 	}
-	/*-------------------------------------------*/
-	/* BreadCrumb
+
+	/*
+	 BreadCrumb
 	/*-------------------------------------------*/
 	$old_file_name[] = 'module_panList.php';
 	if ( locate_template( $old_file_name, false, false ) ) {
@@ -23,7 +24,6 @@ if ( lightning_is_page_header_and_breadcrumb() ){
 	} else {
 		get_template_part( 'template-parts/breadcrumb' );
 	}
-
 } // if ( lightning_is_page_header_and_top_breadcrumb() ){
 ?>
 
@@ -92,7 +92,8 @@ else :
 
 				// tag list
 				$tags_list = get_the_tag_list();
-				if ( $tags_list ) { ?>
+				if ( $tags_list ) {
+					?>
 				<div class="entry-meta-dataList entry-tag">
 					<dl>
 					<dt><?php _e( 'Tags', 'lightning' ); ?></dt>
@@ -118,7 +119,7 @@ else :
 <?php do_action( 'lightning_mainSection_append' ); ?>
 </div><!-- [ /.mainSection ] -->
 
-<?php if ( lightning_is_subsection_display() ){ ?>
+<?php if ( lightning_is_subsection_display() ) { ?>
 	<div class="<?php lightning_the_class_name( 'sideSection' ); ?>">
 	<?php get_sidebar( get_post_type() ); ?>
 	</div><!-- [ /.subSection ] -->
