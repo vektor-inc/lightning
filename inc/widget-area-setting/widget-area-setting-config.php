6bfa9bf275785f187e9d7a86feba8e94a3b76b8c
kurudrive <kurudrive@gmail.com>
2020-05-28 09:12:23 +0900

    [ widget setting ] グローバル変数の定義はclass_existsの条件分岐の中でいいかなー。class-widget-settingの中でしか使わないので。

--- inc/widget-area-setting/widget-area-setting-config.php
+++ inc/widget-area-setting/widget-area-setting-config.php
@@ -7,7 +7,8 @@
 
 if ( ! class_exists( 'Widget_Area_Setting' ) ) {
 	require_once 'package/class-widget-area-setting.php';
-}
 
-global $widget_area_setting_prefix;
-$widget_area_setting_prefix = lightning_get_prefix_customize_panel();
+	global $widget_area_setting_prefix;
+	$widget_area_setting_prefix = lightning_get_prefix_customize_panel();
+
+}
