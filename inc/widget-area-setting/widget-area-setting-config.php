2fe1a052e2f0fcfeb9ae50247d9afa78ec4223b0
rick-addison <rickaddison7634@gmail.com>
2020-06-08 06:12:54 +0900

    dirname( __FILE__ ) . を付与

--- inc/widget-area-setting/widget-area-setting-config.php
+++ inc/widget-area-setting/widget-area-setting-config.php
@@ -6,7 +6,7 @@
  */
 
 if ( ! class_exists( 'Widget_Area_Setting' ) ) {
-	require_once 'package/class-widget-area-setting.php';
+	require_once dirname( __FILE__ ) . '/package/class-widget-area-setting.php';
 
 	global $widget_area_setting_prefix;
 	$widget_area_setting_prefix = lightning_get_prefix_customize_panel();
