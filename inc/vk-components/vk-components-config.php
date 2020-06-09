029fae95605830c56483a7f4313db5e23a324529
Hidekazu Ishikawa <kurudrive@gmail.com>
2020-06-10 04:56:57 +0900

    Update inc/vk-components/vk-components-config.php

--- inc/vk-components/vk-components-config.php
+++ inc/vk-components/vk-components-config.php
@@ -2,10 +2,6 @@
 /*-------------------------------------------*/
 /*  Load modules
 /*-------------------------------------------*/
-global $vk_components_insert_count;
-$vk_components_insert_count = $lightning_insert_count;
-global $vk_components_insert_loop_html;
-$vk_components_insert_loop_html = 'lightning_insert_loop_html';
 
 if ( ! class_exists( 'VK_Component_Button' ) ) {
 	require get_parent_theme_file_path( '/inc/vk-components/package/class-vk-component-button.php' );
