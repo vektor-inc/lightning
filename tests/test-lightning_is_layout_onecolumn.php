2fc363a62cc6c1fb5b0e1997d9d4d9053fd21f4c
rick-addison <rickaddison7634@gmail.com>
2020-06-12 08:57:51 +0900

    テストファイルを修正

--- tests/test-lightning_is_layout_onecolumn.php
+++ tests/test-lightning_is_layout_onecolumn.php
@@ -202,6 +202,16 @@ class LightningIsLayoutOnecolmunTest extends WP_UnitTestCase {
 				'target_url'        => home_url( '/' ) . '?post_type=post',
 				'correct'           => true,
 			),
+			array(
+				'options'           => array(
+					'layout' => array(
+						'archive-event' => 'col-one',
+					),
+				),
+				'_wp_page_template' => '',
+				'target_url'        => home_url( '/' ) . '?post_type=event',
+				'correct'           => true,
+			),
 			// Post type archive
 			array(
 				'options'           => array(
