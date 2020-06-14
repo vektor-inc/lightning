239e59137e8201efaf0f7aa4836d9c9089ce654f
kurudrive <kurudrive@gmail.com>
2020-06-15 04:00:46 +0900

    [ 仕様変更 ] page.php と single.php を singular.php に統合し、中の部分は content-page.php などに分離

---
+++ template-parts/post/content-page.php
@@ -0,0 +1,26 @@
+<?php
+if ( have_posts() ) {
+	while ( have_posts() ) :
+		the_post(); ?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class( apply_filters( 'lightning_article_outer_class', '' ) ); ?>>
+
+		<?php do_action( 'lightning_entry_body_before' ); ?>
+		<div class="<?php lightning_the_class_name( 'entry-body' ); ?>">
+		<?php the_content(); ?>
+		</div>
+		<?php do_action( 'lightning_entry_body_after' ); ?>
+
+		<?php
+		$args = array(
+			'before'      => '<nav class="page-link"><dl><dt>Pages :</dt><dd>',
+			'after'       => '</dd></dl></nav>',
+			'link_before' => '<span class="page-numbers">',
+			'link_after'  => '</span>',
+			'echo'        => 1,
+		);
+		wp_link_pages( $args );
+		?>
+		</article><!-- [ /#post-<?php the_ID(); ?> ] -->
+	<?php endwhile;
+}
+?>
