ea6c685e3b900e6baf5723f96c57c2be67537c50
Shoji Imamura <imamura@vektor-inc.co.jp>
2020-06-15 12:12:45 +0900

    モバイル化

--- inc/vk-mobile-nav/package/js/vk-mobile-nav.js
+++ inc/vk-mobile-nav/package/js/vk-mobile-nav.js
@@ -203,3 +203,12 @@ If you want to change this file that, you have to change original file.
         document.addEventListener('DOMContentLoaded', vk_menu_acc_run)
     })(5000);
 })(window, document);
+
+((d)=>{
+    const mobile = require('is-mobile');
+    window.addEventListener('DOMContentLoaded', ()=>{
+        const isMobile = mobile.isMobile();
+        ['device-mobile', 'device-pc'].forEach((m)=>document.body.classList.remove(m))
+        document.body.classList.add(mobile.isMobile()? 'device-mobile': 'device-pc')
+    })
+})(document)
