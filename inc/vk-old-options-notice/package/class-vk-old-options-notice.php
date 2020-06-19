f37e7ac47e9914261bb7731c642564ee17d02ccb
rick-addison <rickaddison7634@gmail.com>
2020-06-19 10:46:44 +0900

    基本形と判定関数のみ

---
+++ inc/vk-old-options-notice/package/class-vk-old-options-notice.php
@@ -0,0 +1,34 @@
+<?
+/**
+ * VK Old Options Notice
+ *
+ * @package Lightning
+ */
+
+/**
+ * VK Old Options Notice
+ */
+class VK_Old_Options_Notice {
+
+	public function __construct() {
+		add_action( 'admin_notices', 'admin_notice' );
+		add_action( 'admin_init', 'launch_update_options');
+	}
+
+	public static function option_judgment( $arg ) {
+		global $old_setting_array;
+		foreach ( (array) $old_setting_array as $old_setting ) {
+			if ( 'option' === $old_setting['data_type'] ) {
+				$options = get_option( $old_setting['target_field'] );
+				if ( isset( $options['old_value'] ) ) {
+					if ( 'judge' === $arg ) {
+						return true;
+					} else {
+						call_user_func( $options['callback'] );
+					}
+				}
+			}
+		}
+	}
+	
+}
---
+++ inc/vk-old-options-notice/vk-old-options-notice-config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * VK Old Options Notice Config
+ *
+ * @package Lightning
+ */
+
+if ( class_exists( 'VK_Old_Options_Notice' ) ) {
+global $old_setting_array;
+$old_setting_array = array(
+	array(
+		'data_type' => 'option',
+		'target_field' => 'lightning_theme_option',
+		'old_value' => array(
+			'layout' => array(
+				'archive' => 'one-column',
+			)
+		),
+		'callback' => 'lightning_options_compatible',
+	),
+);
+
+require_once dirname( __FILE__ ) . 'package/class-vk-old-notice.php';
+}
