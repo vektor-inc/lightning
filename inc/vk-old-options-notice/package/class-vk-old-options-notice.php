fd59e0389d186e37b65112859f16679c3717427b
rick-addison <rickaddison7634@gmail.com>
2020-06-22 12:42:45 +0900

    とりあえず完成？

--- inc/vk-old-options-notice/package/class-vk-old-options-notice.php
+++ inc/vk-old-options-notice/package/class-vk-old-options-notice.php
@@ -14,7 +14,12 @@ class VK_Old_Options_Notice {
 		add_action( 'admin_notices', 'admin_notice' );
 		add_action( 'admin_init', 'launch_update_options');
 	}
-
+	/**
+	 * Option Judgment
+	 *
+	 * @param string $arg judge or action
+	 * @return void
+	 */
 	public static function option_judgment( $arg ) {
 		global $old_setting_array;
 		foreach ( (array) $old_setting_array as $old_setting ) {
@@ -23,11 +28,30 @@ class VK_Old_Options_Notice {
 				if ( isset( $options['old_value'] ) ) {
 					if ( 'judge' === $arg ) {
 						return true;
-					} else {
+					} elseif ( 'action' === $arg )   {
 						call_user_func( $options['callback'] );
 					}
 				}
 			}
 		}
 	}
+
+	public static function admin_notice() {
+		global $pagenow;
+		global $vk_update_link;
+		if ( 'index.php' === $pagenow ) {
+			if ( option_judgment( 'judge' ) ) {
+				echo '<div class="notice notice-info"><p>' . __( 'Because old option is exists, you need to update detabase', 'lightning' ) . '<a href="' . $vk_update_link . '">' . __( 'Update database', 'lightning' ) . '</a></p></div>';
+
+			}
+		}
+	}
+
+	public static function launch_update_options() {
+		global $vk_update_link;
+		if( isset( $_GET[ $vk_update_link ] ) ) {
+			option_judgment( 'action' );
+		}
+	}
+
 }
--- inc/vk-old-options-notice/vk-old-options-notice-config.php
+++ inc/vk-old-options-notice/vk-old-options-notice-config.php
@@ -6,19 +6,22 @@
  */
 
 if ( class_exists( 'VK_Old_Options_Notice' ) ) {
-global $old_setting_array;
-$old_setting_array = array(
-	array(
-		'data_type' => 'option',
-		'target_field' => 'lightning_theme_option',
-		'old_value' => array(
-			'layout' => array(
-				'archive' => 'one-column',
-			)
+	global $old_setting_array;
+	$old_setting_array = array(
+		array(
+			'data_type'    => 'option',
+			'target_field' => 'lightning_theme_option',
+			'old_value'    => array(
+				'layout' => array(
+					'archive' => 'one-column',
+				),
+			),
+			'callback'     => 'lightning_options_compatible',
 		),
-		'callback' => 'lightning_options_compatible',
-	),
-);
+	);
 
-require_once dirname( __FILE__ ) . 'package/class-vk-old-notice.php';
+	global $vk_update_link;
+	$vk_update_link = '?lightning-update-link';
+
+	require_once dirname( __FILE__ ) . '/package/class-vk-old-notice.php';
 }
