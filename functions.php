7097b68848aabfd37c7dd5bfe8673bd6a0860967
rick-addison <rickaddison7634@gmail.com>
2020-07-02 14:56:27 +0900

    コミット忘れ

--- functions.php
+++ functions.php
@@ -238,6 +238,7 @@ require get_parent_theme_file_path( '/inc/vk-components/vk-components-config.php
 require get_parent_theme_file_path( '/inc/template-redirect.php' );
 require get_parent_theme_file_path( '/inc/layout-controller/layout-controller.php' );
 require get_parent_theme_file_path( '/inc/widget-area-setting/widget-area-setting-config.php' );
+require get_parent_theme_file_path( '/inc/vk-css-preload/vk-css-preload-config.php' );
 
 /*
   Load woocommerce modules
