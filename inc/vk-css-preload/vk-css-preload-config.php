4a45342e73c32eec7dce20301c41b657943f84b1
rick-addison <rickaddison7634@gmail.com>
2020-07-02 15:01:58 +0900

    エラーを修正

--- inc/vk-css-preload/vk-css-preload-config.php
+++ inc/vk-css-preload/vk-css-preload-config.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * VK CSS Preload Config
- * 
+ *
  * @package Lightning
  */
 
 if ( ! class_exists( 'VK_CSS_Preload' ) ) {
-	require_once dirname( __FILE__ ) . '/class-vk-css-preload.php';
+	require_once dirname( __FILE__ ) . '/package/class-vk-css-preload.php';
 }
