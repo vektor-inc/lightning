5828c1c06c0860ccf93a67e3cba8aea319fbe2c9
rick-addison <rickaddison7634@gmail.com>
2020-07-16 16:58:08 +0900

    少し調整

--- inc/vk-css-optimize/package/class-vk-css-optimize.php
+++ inc/vk-css-optimize/package/class-vk-css-optimize.php
@@ -45,24 +45,24 @@ if ( ! class_exists( 'VK_CSS_Optimize' ) ) {
 			ob_end_flush();
 		}
 
-		public static function curl_get_contents($url, $timeout = 30){
+		public static function curl_get_contents( $url, $timeout = 30 ) {
 
 			$ch = curl_init();
 			curl_setopt( $ch, CURLOPT_URL, $url );
 			curl_setopt( $ch, CURLOPT_HEADER, false );
 			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-			curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER,false);
-		
+			curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+
 			//タイムアウト時間設定
 			curl_setopt( $ch, CURLOPT_TIMEOUT, $timeout );
-			
+
 			//リダイレクトしている場合も読みこむ
 			curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
 			curl_setopt( $ch, CURLOPT_MAXREDIRS, 10);
-		
+
 			$result = curl_exec( $ch );
 			curl_close( $ch );
-		
+
 			return $result;
 		}
 
