a194009c580c64a777b726f7f66b1a79eb6abeee
rick-addison <rickaddison7634@gmail.com>
2020-07-27 11:53:42 +0900

    デフォルト状態だと色々おかしかったのを修正

--- design-skin/origin2/origin2.php
+++ design-skin/origin2/origin2.php
@@ -1,39 +1,41 @@
 <?php
+/*
+-------------------------------------------*/
+/*
+  Print head
 /*-------------------------------------------*/
-/*	Print head
-/*-------------------------------------------*/
-add_action( 'wp_head', 'lightning_print_css_origin', 3 );
+add_action( 'wp_head', 'lightning_print_css_origin', 5 );
 function lightning_print_css_origin() {
 	$options = get_option( 'lightning_theme_options' );
-	if ( isset( $options['color_key'] ) && isset( $options['color_key_dark'] ) ) {
-		$color_key      = esc_html( $options['color_key'] );
-		$color_key_dark = esc_html( $options['color_key_dark'] );
 
-		// Text Color ///////////////////
-		$dynamic_css = '.media .media-body .media-heading a:hover { color:' . $color_key . ';  }';
+	$color_key      = ! empty( $options['color_key'] ) ? esc_html( $options['color_key'] ) : '#337ab7';
+	$color_key_dark = ! empty( $options['color_key_dark'] ) ? esc_html( $options['color_key_dark'] ) : '#2e6da4';
 
-		// Global Menu //////////////////
-		$dynamic_css .= '
+	// Text Color ///////////////////
+	$dynamic_css = '.media .media-body .media-heading a:hover { color:' . $color_key . ';  }';
+
+	// Global Menu //////////////////
+	$dynamic_css .= '
+	@media (min-width: 768px){
+		.gMenu > li:before,
+		.gMenu > li.menu-item-has-children::after { border-bottom-color:' . $color_key_dark . ' }
+		.gMenu li li { background-color:' . $color_key_dark . ' }
+		.gMenu li li a:hover { background-color:' . $color_key . '; }
+	} /* @media (min-width: 768px) */';
+	if ( ! empty( $options['color_header_bg'] ) ) {
+		$color_header_bg = esc_html( $options['color_header_bg'] );
+		if ( lightning_check_color_mode( $color_header_bg ) == 'dark' ) {
+			// Dark Color ///////////////////
+			$dynamic_css .= '
 		@media (min-width: 768px){
-			.gMenu > li:before,
-			.gMenu > li.menu-item-has-children::after { border-bottom-color:' . $color_key_dark . ' }
-			.gMenu li li { background-color:' . $color_key_dark . ' }
-			.gMenu li li a:hover { background-color:' . $color_key . '; }
-		} /* @media (min-width: 768px) */';
-		if ( ! empty( $options['color_header_bg'] ) ) {
-			$color_header_bg = esc_html( $options['color_header_bg'] );
-			if ( lightning_check_color_mode( $color_header_bg ) == 'dark' ) {
-				// Dark Color ///////////////////
-				$dynamic_css .= '
-			@media (min-width: 768px){
-				ul.gMenu > li > a:after { border-bottom-color: rgba(255,255,255,0.9 );}
-			}';
-			} else {
-				// Light Color ///////////////////
-			}
-		}// if ( ! empty( $options['color_header_bg'] ) ) {
+			ul.gMenu > li > a:after { border-bottom-color: rgba(255,255,255,0.9 );}
+		}';
+		} else {
+			// Light Color ///////////////////
+		}
+	}// if ( ! empty( $options['color_header_bg'] ) ) {
 
-		$dynamic_css .= '
+	$dynamic_css .= '
 .page-header { background-color:' . $color_key . '; }
 h2,
 .mainSection-title { border-top-color:' . $color_key . '; }
@@ -45,28 +47,29 @@ ul.page-numbers li span.page-numbers.current { background-color:' . $color_key .
 .siteFooter { border-top-color:' . $color_key . '; }
 dt { border-left-color:' . $color_key . '; }
 :root {
-	--g_nav_main_acc_icon_open_url:url('.get_template_directory_uri(  ).'/inc/vk-mobile-nav/package/images/vk-menu-acc-icon-open-black.svg);
-	--g_nav_main_acc_icon_close_url: url('.get_template_directory_uri(  ).'/inc/vk-mobile-nav/package/images/vk-menu-close-black.svg);
-	--g_nav_sub_acc_icon_open_url: url('.get_template_directory_uri(  ).'/inc/vk-mobile-nav/package/images/vk-menu-acc-icon-open-white.svg);
-	--g_nav_sub_acc_icon_close_url: url('.get_template_directory_uri(  ).'/inc/vk-mobile-nav/package/images/vk-menu-close-white.svg);
+	--g_nav_main_acc_icon_open_url:url(' . get_template_directory_uri() . '/inc/vk-mobile-nav/package/images/vk-menu-acc-icon-open-black.svg);
+	--g_nav_main_acc_icon_close_url: url(' . get_template_directory_uri() . '/inc/vk-mobile-nav/package/images/vk-menu-close-black.svg);
+	--g_nav_sub_acc_icon_open_url: url(' . get_template_directory_uri() . '/inc/vk-mobile-nav/package/images/vk-menu-acc-icon-open-white.svg);
+	--g_nav_sub_acc_icon_close_url: url(' . get_template_directory_uri() . '/inc/vk-mobile-nav/package/images/vk-menu-close-white.svg);
 }
 ';
 
-		// delete before after space
-		$dynamic_css = trim( $dynamic_css );
-		// convert tab and br to space
-		$dynamic_css = preg_replace( '/[\n\r\t]/', '', $dynamic_css );
-		// Change multiple spaces to single space
-		$dynamic_css = preg_replace( '/\s(?=\s)/', '', $dynamic_css );
-		wp_add_inline_style( 'lightning-design-style', $dynamic_css );
-	} // if ( isset($options['color_key'] && isset($options['color_key_dark'] ) {
+	// delete before after space
+	$dynamic_css = trim( $dynamic_css );
+	// convert tab and br to space
+	$dynamic_css = preg_replace( '/[\n\r\t]/', '', $dynamic_css );
+	// Change multiple spaces to single space
+	$dynamic_css = preg_replace( '/\s(?=\s)/', '', $dynamic_css );
+	wp_add_inline_style( 'lightning-design-style', $dynamic_css );
 }
 
-/*-------------------------------------------*/
-/*	Your design skin Specific functions
+/*
+-------------------------------------------*/
+/*
+  Your design skin Specific functions
 /*-------------------------------------------*/
 
-add_filter('lightning_localize_options', 'lightning_origin2_add_js_option', 10 , 1);
+add_filter( 'lightning_localize_options', 'lightning_origin2_add_js_option', 10, 1 );
 function lightning_origin2_add_js_option( $options ) {
 	$options['header_scrool'] = true;
 	return $options;
