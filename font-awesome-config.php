671c9576db5ac974c9044936854c46e1beaca87f
rick-addison <rickaddison7634@gmail.com>
2020-08-28 16:09:26 +0900

    1st commit

---
+++ font-awesome-config.php
@@ -0,0 +1,13 @@
+<?php
+/*-------------------------------------------*/
+/*  Load modules
+/*-------------------------------------------*/
+if ( ! class_exists( 'Vk_Font_Awesome_Versions' ) ) {
+	require_once( 'font-awesome/class-vk-font-awesome-versions.php' );
+
+	global $font_awesome_directory_uri;
+	$font_awesome_directory_uri = plugins_url( '', __FILE__ ) . '/font-awesome/';
+
+	global $set_enqueue_handle_style;
+	$set_enqueue_handle_style = 'vkExUnit_common_style';
+}
