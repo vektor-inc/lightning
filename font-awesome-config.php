9a14aecb9140a2e1f2190d83ff3620794dc2c0b2
rick-addison <rickaddison7634@gmail.com>
2020-09-01 14:09:08 +0900

    少し修正

--- font-awesome-config.php
+++ font-awesome-config.php
@@ -3,7 +3,7 @@
 /*  Load modules
 /*-------------------------------------------*/
 if ( ! class_exists( 'Vk_Font_Awesome_Versions' ) ) {
-	require_once( 'font-awesome/class-vk-font-awesome-versions.php' );
+	require_once 'package/class-vk-font-awesome-versions.php';
 
 	global $font_awesome_directory_uri;
 	$font_awesome_directory_uri = plugins_url( '', __FILE__ ) . '/font-awesome/';
