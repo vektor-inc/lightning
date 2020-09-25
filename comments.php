5dde99903c1bd88b1ddf347ecd89add2c70e1a0b
drill-lancer <rickaddison7634@gmail.com>
2020-09-26 06:02:03 +0900

    インデント付加

--- comments.php
+++ comments.php
@@ -17,66 +17,68 @@ if ( post_password_required() ) {
 	return;
 }
 $options = get_option( 'lightning_theme_options' );
-$deahult = lightning_default_comment_options();
-$options = wp_parse_args( $options, $deahult );
+$default = lightning_default_comment_options();
+$options = wp_parse_args( $options, $default );
 ?>
-<?php if ( empty( $options['hide_comment'][get_post_type()] ) && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-<div id="comments" class="comments-area">
+<?php if ( empty( $options['hide_comment'][ get_post_type() ] ) && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+	<div id="comments" class="comments-area">
 
-	<?php // You can start editing here -- including this comment! ?>
+		<?php // You can start editing here -- including this comment! ?>
 
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'lightning' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-			?>
-		</h2>
+		<?php if ( have_comments() ) : ?>
+			<h2 class="comments-title">
+				<?php
+					printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'lightning' ),
+						number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+				?>
+			</h2>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php _e( 'Comment navigation', 'lightning' ); ?></h2>
-			<div class="nav-links">
+			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+				<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
+					<h2 class="screen-reader-text"><?php _e( 'Comment navigation', 'lightning' ); ?></h2>
+					<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( __( 'Older Comments', 'lightning' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments', 'lightning' ) ); ?></div>
+						<div class="nav-previous"><?php previous_comments_link( __( 'Older Comments', 'lightning' ) ); ?></div>
+						<div class="nav-next"><?php next_comments_link( __( 'Newer Comments', 'lightning' ) ); ?></div>
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // check for comment navigation ?>
+					</div><!-- .nav-links -->
+				</nav><!-- #comment-nav-above -->
+			<?php endif; // check for comment navigation ?>
 
-		<ol class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				) );
-			?>
-		</ol><!-- .comment-list -->
+			<ol class="comment-list">
+				<?php
+					wp_list_comments(
+						array(
+							'style'      => 'ol',
+							'short_ping' => true,
+						)
+					);
+				?>
+			</ol><!-- .comment-list -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php _e( 'Comment navigation', 'lightning' ); ?></h2>
-			<div class="nav-links">
+			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+				<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
+					<h2 class="screen-reader-text"><?php _e( 'Comment navigation', 'lightning' ); ?></h2>
+					<div class="nav-links">
 
-				<div class="nav-previous"><?php previous_comments_link( __( 'Older Comments', 'lightning' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments', 'lightning' ) ); ?></div>
+						<div class="nav-previous"><?php previous_comments_link( __( 'Older Comments', 'lightning' ) ); ?></div>
+						<div class="nav-next"><?php next_comments_link( __( 'Newer Comments', 'lightning' ) ); ?></div>
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-below -->
-		<?php endif; // check for comment navigation ?>
+					</div><!-- .nav-links -->
+				</nav><!-- #comment-nav-below -->
+			<?php endif; // check for comment navigation ?>
 
-	<?php endif; // have_comments() ?>
+		<?php endif; // have_comments() ?>
 
-	<?php
+		<?php
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() && '0' != get_comments_number() ) :
-	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'lightning' ); ?></p>
+		?>
+			<p class="no-comments"><?php _e( 'Comments are closed.', 'lightning' ); ?></p>
 
-	<?php endif; ?>
+		<?php endif; ?>
 
-	<?php comment_form(); ?>
+		<?php comment_form(); ?>
 
-</div><!-- #comments -->
+	</div><!-- #comments -->
 <?php endif; ?>
