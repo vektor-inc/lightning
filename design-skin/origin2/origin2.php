3c06a7d4d23f119d97dbfbac27d1e13c1c2b6469
goutetsuguma <goutetsuguma@gmail.com>
2020-10-05 15:41:49 +0900

    改ページブロックのcssがあたっていないのを修正

--- design-skin/origin2/origin2.php
+++ design-skin/origin2/origin2.php
@@ -47,7 +47,8 @@ h2,
 .mainSection-title { border-top-color:' . $color_key . '; }
 h3:after,
 .subSection-title:after { border-bottom-color:' . $color_key . ';  }
-ul.page-numbers li span.page-numbers.current { background-color:' . $color_key . '; }
+ul.page-numbers li span.page-numbers.current,
+.page-link dl .post-page-numbers.current { background-color:' . $color_key . '; }
 .pager li > a { border-color:' . $color_key . ';color:' . $color_key . ';}
 .pager li > a:hover { background-color:' . $color_key . ';color:#fff;}
 .siteFooter { border-top-color:' . $color_key . '; }
