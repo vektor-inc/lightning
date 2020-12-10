b918c1cc2313737aa88072f2a09bddb78d50609e
drill-lancer <rickaddison7634@gmail.com>
2020-12-10 13:20:00 +0900

    fix-footer-setting

--- inc/vk-footer-customize/package/class-widget-area-setting.php
+++ inc/vk-footer-customize/package/class-widget-area-setting.php
@@ -2,7 +2,7 @@
 /**
  * Widget Setting
  *
- * @package Lightning
+ * @package VK Footer Customize
  */
 
 if ( ! class_exists( 'Widget_Area_Setting' ) ) {
@@ -14,9 +14,21 @@ if ( ! class_exists( 'Widget_Area_Setting' ) ) {
 		 * Constructor.
 		 */
 		public function __construct() {
+			global $vk_footer_setting_name;
 			add_action( 'customize_register', array( __CLASS__, 'resister_customize' ) );
 			add_action( 'wp_head', array( __CLASS__, 'enqueue_style' ), 5 );
-			add_filter( 'lightning_footer_widget_area_count', array( __CLASS__, 'set_footter_widget_area_count' ) );
+			add_filter( $vk_footer_setting_name . '_footer_widget_area_count', array( __CLASS__, 'set_footter_widget_area_count' ) );
+		}
+
+		/**
+		 * Default Options
+		 */
+		public static function default_options() {
+			$default = array(
+				'footer_upper_widget_padding_delete' => 'false',
+				'footer_widget_area_count'           => 3,
+			);
+			return $default;
 		}
 
 		/**
@@ -25,6 +37,10 @@ if ( ! class_exists( 'Widget_Area_Setting' ) ) {
 		 * @param \WP_Customize_Manager $wp_customize Customizer.
 		 */
 		public static function resister_customize( $wp_customize ) {
+			$default = self::default_options();
+
+			global $vk_footer_selector;
+			global $vk_footer_setting_name;
 			global $vk_footer_customize_prefix;
 			global $vk_footer_customize_priority;
 			if ( ! $vk_footer_customize_priority ) {
@@ -36,7 +52,7 @@ if ( ! class_exists( 'Widget_Area_Setting' ) ) {
 			$wp_customize->add_section(
 				'vk_footer_option',
 				array(
-					'title'    => $vk_footer_customize_prefix . __( 'Footer settings', 'lightning' ),
+					'title'    => $vk_footer_customize_prefix . __( 'Footer settings', 'vk-footer-customize-textdomain' ),
 					'priority' => $vk_footer_customize_priority,
 				)
 			);
@@ -48,13 +64,12 @@ if ( ! class_exists( 'Widget_Area_Setting' ) ) {
 					'sanitize_callback' => 'sanitize_text_field',
 				)
 			);
-
 			$wp_customize->add_control(
 				new Custom_Html_Control(
 					$wp_customize,
 					'footer-widget-setting',
 					array(
-						'label'            => __( 'Footer Widget Setting', 'lightning' ),
+						'label'            => __( 'Footer Widget Setting', 'vk-footer-customize-textdomain' ),
 						'section'          => 'vk_footer_option',
 						'type'             => 'text',
 						'custom_title_sub' => '',
@@ -66,41 +81,39 @@ if ( ! class_exists( 'Widget_Area_Setting' ) ) {
 
 			// Padding Bottom.
 			$wp_customize->add_setting(
-				'lightning_widget_setting[footer_upper_widget_padding_delete]',
+				$vk_footer_setting_name . '[footer_upper_widget_padding_delete]',
 				array(
-					'default'           => false,
+					'default'           => 'false',
 					'type'              => 'option',
 					'capability'        => 'edit_theme_options',
 					'sanitize_callback' => array( 'VK_Helpers', 'sanitize_choice' ),
 				)
 			);
-
 			$wp_customize->add_control(
-				'lightning_widget_setting[footer_upper_widget_padding_delete]',
+				$vk_footer_setting_name . '[footer_upper_widget_padding_delete]',
 				array(
-					'label'    => __( 'Footer Upper Widget Padding', 'lightning' ),
+					'label'    => __( 'Footer Upper Widget Padding', 'vk-footer-customize-textdomain' ),
 					'section'  => 'vk_footer_option',
-					'settings' => 'lightning_widget_setting[footer_upper_widget_padding_delete]',
+					'settings' => $vk_footer_setting_name . '[footer_upper_widget_padding_delete]',
 					'type'     => 'select',
 					'choices'  => array(
-						false => __( 'Nothing to do', 'lightning' ),
-						true  => __( 'Delete Padding', 'lightning' ),
+						'false' => __( 'Nothing to do', 'vk-footer-customize-textdomain' ),
+						'true'  => __( 'Delete Padding', 'vk-footer-customize-textdomain' ),
 					),
 					'priority' => $priority,
 				)
 			);
-
 			$wp_customize->selective_refresh->add_partial(
-				'lightning_widget_setting[footer_upper_widget_padding_delete]',
+				$vk_footer_setting_name . '[footer_upper_widget_padding_delete]',
 				array(
-					'selector'        => '.siteContent_after',
+					'selector'        => $vk_footer_selector,
 					'render_callback' => '',
 				)
 			);
 
 			// Number of Footer Widget area.
 			$wp_customize->add_setting(
-				'lightning_widget_setting[footer_widget_area_count]',
+				$vk_footer_setting_name . '[footer_widget_area_count]',
 				array(
 					'default'           => 3,
 					'type'              => 'option',
@@ -108,30 +121,28 @@ if ( ! class_exists( 'Widget_Area_Setting' ) ) {
 					'sanitize_callback' => array( 'VK_Helpers', 'sanitize_number' ),
 				)
 			);
-
 			$wp_customize->add_control(
-				'lightning_widget_setting[footer_widget_area_count]',
+				$vk_footer_setting_name . '[footer_widget_area_count]',
 				array(
-					'label'       => __( 'Footer Widget Area Count', 'lightning' ),
+					'label'       => __( 'Footer Widget Area Count', 'vk-footer-customize-textdomain' ),
 					'section'     => 'vk_footer_option',
-					'settings'    => 'lightning_widget_setting[footer_widget_area_count]',
+					'settings'    => $vk_footer_setting_name . '[footer_widget_area_count]',
 					'type'        => 'select',
 					'choices'     => array(
-						1 => __( '1 column', 'lightning' ),
-						2 => __( '2 column', 'lightning' ),
-						3 => __( '3 column', 'lightning' ),
-						4 => __( '4 column', 'lightning' ),
-						6 => __( '6 column', 'lightning' ),
+						1 => __( '1 column', 'vk-footer-customize-textdomain' ),
+						2 => __( '2 column', 'vk-footer-customize-textdomain' ),
+						3 => __( '3 column', 'vk-footer-customize-textdomain' ),
+						4 => __( '4 column', 'vk-footer-customize-textdomain' ),
+						6 => __( '6 column', 'vk-footer-customize-textdomain' ),
 					),
-					'description' => __( '* If you save and reload after making changes, the number of the widget area setting panels  will increase or decrease.', 'lightning' ),
+					'description' => __( '* If you save and reload after making changes, the number of the widget area setting panels  will increase or decrease.', 'vk-footer-customize-textdomain' ),
 					'priority'    => $priority,
 				)
 			);
-
 			$wp_customize->selective_refresh->add_partial(
-				'lightning_widget_setting[footer_widget_area_count]',
+				$vk_footer_setting_name . '[footer_widget_area_count]',
 				array(
-					'selector'        => '.footerWidget',
+					'selector'        => $vk_footer_selector,
 					'render_callback' => '',
 				)
 			);
@@ -141,14 +152,21 @@ if ( ! class_exists( 'Widget_Area_Setting' ) ) {
 		 * Enqueue Style.
 		 */
 		public static function enqueue_style() {
-			$options     = get_option( 'lightning_widget_setting' );
+			global $vk_footer_selector;
+			global $vk_footer_setting_name;
+			global $vk_footer_customize_hook_style;
+
+			$options = get_option( $vk_footer_setting_name );
+			$default = self::default_options();
+			$options = wp_parse_args( $options, $default );
+
 			$dynamic_css = '';
-			if ( ! empty( $options['footer_upper_widget_padding_delete'] ) ) {
-				$dynamic_css  = '.siteContent_after.sectionBox{';
-				$dynamic_css .= 'padding:0';
+			if ( 'true' === $options['footer_upper_widget_padding_delete'] ) {
+				$dynamic_css  = $vk_footer_selector . '{';
+				$dynamic_css .= 'padding:0;';
 				$dynamic_css .= '}';
 			}
-			wp_add_inline_style( 'lightning-design-style', $dynamic_css );
+			wp_add_inline_style( $vk_footer_customize_hook_style, $dynamic_css );
 		}
 
 		/**
@@ -157,8 +175,9 @@ if ( ! class_exists( 'Widget_Area_Setting' ) ) {
 		 * @param int $footer_widget_area_count Footer Widget Area Count.
 		 */
 		public static function set_footter_widget_area_count( $footer_widget_area_count ) {
+			global $vk_footer_setting_name;
 			$footer_widget_area_count = 3;
-			$options                  = get_option( 'lightning_widget_setting' );
+			$options                  = get_option( $vk_footer_setting_name );
 			if ( ! empty( $options['footer_widget_area_count'] ) ) {
 				$footer_widget_area_count = (int) $options['footer_widget_area_count'];
 			}
--- inc/vk-footer-customize/vk-footer-customize-config.php
+++ inc/vk-footer-customize/vk-footer-customize-config.php
@@ -1,18 +1,24 @@
 <?php
 /**
- * Footer Customize Config
+ * VK Footer Customize Config
  *
- * @package Lightning Pro
+ * @package VK Footer Customize
  */
 
 if ( ! class_exists( 'VK_Footer_Customize' ) ) {
 	global $vk_footer_customize_hook_style;
 	$vk_footer_customize_hook_style = 'lightning-design-style';
 
+	global $vk_footer_selector;
+	$vk_footer_selector = '.siteContent_after.sectionBox';
+
 	require_once dirname( __FILE__ ) . '/package/class-widget-area-setting.php';
 }
 global $vk_footer_customize_prefix;
 $vk_footer_customize_prefix = lightning_get_prefix_customize_panel();
+
+global $vk_footer_setting_name;
+$vk_footer_setting_name = 'lightning_widget_setting';
 
 global $vk_footer_customize_priority;
 $vk_footer_customize_priority = 540;
