690e83fb0bac6e5b47401063dc602e16d1764291
kurudrive <kurudrive@gmail.com>
2020-12-22 04:23:08 +0900

    [ G3 ] パンくず調整

--- _g3/inc/vk-breadcrumb/package/class-vk-breadcrumb.php
+++ _g3/inc/vk-breadcrumb/package/class-vk-breadcrumb.php
@@ -154,133 +154,137 @@ if ( ! class_exists( 'VK_Breadcrumb' ) ) {
                 // Get category information & insert to $cat
                 $cat = get_queried_object();
         
-        // parent != 0  >>>  Parent exist
-        if ( $cat->parent != 0 ) :
-            // 祖先のカテゴリー情報を逆順で取得
-            $ancestors = array_reverse( get_ancestors( $cat->cat_ID, 'category' ) );
-            // 祖先階層の配列回数分ループ
-            foreach ( $ancestors as $ancestor ) :
+                // parent != 0  >>>  Parent exist
+                if ( $cat->parent != 0 ) {
+                    // 祖先のカテゴリー情報を逆順で取得
+                    $ancestors = array_reverse( get_ancestors( $cat->cat_ID, 'category' ) );
+                    // 祖先階層の配列回数分ループ
+                    foreach ( $ancestors as $ancestor ) :
+                        $breadcrumb_array[] = array(
+                            'name'             => get_cat_name( $ancestor ),
+                            'id'               => '',
+                            'url'              => get_category_link( $ancestor ),
+                            'class'            => '',
+                        );
+                    endforeach;
+                }
                 $breadcrumb_array[] = array(
-                    'name'             => get_cat_name( $ancestor ),
+                    'name'             => $cat->cat_name,
                     'id'               => '',
-                    'url'              => get_category_link( $ancestor ),
+                    'url'              => '',
                     'class'            => '',
                 );
-            endforeach;
-            endif;
-            $breadcrumb_array[] = array(
-                'name'             => $cat->cat_name,
-                'id'               => '',
-                'url'              => '',
-                'class'            => '',
-            );
         
-        } elseif ( is_tax() ) {
-        
-            /* term
-            /*-------------------------------*/
-            $now_term        = $wp_query->queried_object->term_id;
-            $now_term_parent = $wp_query->queried_object->parent;
-            $now_taxonomy    = $wp_query->queried_object->taxonomy;
+            } elseif ( is_tax() ) {
             
-            // parent が !0 の場合 = 親カテゴリーが存在する場合
-            if ( $now_term_parent != 0 ) :
-                // 祖先のカテゴリー情報を逆順で取得
-                $ancestors = array_reverse( get_ancestors( $now_term, $now_taxonomy ) );
-                // 祖先階層の配列回数分ループ
-                foreach ( $ancestors as $ancestor ) :
-                    $pan_term           = get_term( $ancestor, $now_taxonomy );
-                    $breadcrumb_array[] = array(
-                        'name'             => esc_html( $pan_term->name ),
-                        'id'               => '',
-                        'url'              => get_term_link( $ancestor, $now_taxonomy ),
-                        'class'            => '',
-                    );
-                endforeach;
-                endif;
-            $breadcrumb_array[] = array(
-                'name'             => single_cat_title( '', '', false ),
-                'id'               => '',
-                'url'              => '',
-                'class'            => '',
-            );
-
-        } elseif ( is_single() ) {
-        
-            /* Single
-            /*-------------------------------*/
-            
-            // Case of post
-            
-            if ( $post_type_info['slug'] == 'post' ) {
-                $category = get_the_category();
-                // get parent category info
-                $parents = array_reverse( get_ancestors( $category[0]->term_id, 'category', 'taxonomy' ) );
-                array_push( $parents, $category[0]->term_id );
-                foreach ( $parents as $parent_term_id ) {
-                    $parent_obj         = get_term( $parent_term_id, 'category' );
-                    $term_url           = get_term_link( $parent_obj->term_id, $parent_obj->taxonomy );
-                    $breadcrumb_array[] = array(
-                        'name'             => $parent_obj->name,
-                        'id'               => '',
-                        'url'              => $term_url,
-                        'class'            => '',
-                    );
-                }
-            
-            // Case of custom post type
-            
-            } else {
-
-                // Taxonomy of Single or Page.
-                $get_taxonomies = get_the_taxonomies();
-                $taxonomies     = array();
-
-                // 一旦タクソノミーの文字列配列に変換しないと色々と面倒.
-                foreach ( $get_taxonomies as $key => $value ) {
-                    $taxonomies[] = $key;
-                }
-
-                // 除外するタクソノミーの文字列配列.
-                $exclude_taxonomies = array(
-                    'product_type',
-                    'language', // Polylang その１.
-                    'post_translations',// Polylang その２.
+                /* term
+                /*-------------------------------*/
+                $now_term        = $wp_query->queried_object->term_id;
+                $now_term_parent = $wp_query->queried_object->parent;
+                $now_taxonomy    = $wp_query->queried_object->taxonomy;
+                
+                // parent が !0 の場合 = 親カテゴリーが存在する場合
+                if ( $now_term_parent != 0 ) :
+                    // 祖先のカテゴリー情報を逆順で取得
+                    $ancestors = array_reverse( get_ancestors( $now_term, $now_taxonomy ) );
+                    // 祖先階層の配列回数分ループ
+                    foreach ( $ancestors as $ancestor ) :
+                        $pan_term           = get_term( $ancestor, $now_taxonomy );
+                        $breadcrumb_array[] = array(
+                            'name'             => esc_html( $pan_term->name ),
+                            'id'               => '',
+                            'url'              => get_term_link( $ancestor, $now_taxonomy ),
+                            'class'            => '',
+                        );
+                    endforeach;
+                    endif;
+                $breadcrumb_array[] = array(
+                    'name'             => single_cat_title( '', '', false ),
+                    'id'               => '',
+                    'url'              => '',
+                    'class'            => '',
                 );
-                $exclude_taxonomies = apply_filters( 'vk_breadcrumb_exlude_taxonomy', $exclude_taxonomies );
 
-                // タクソノミーの差分を採用.
-                $taxonomies         = array_diff( $taxonomies, $exclude_taxonomies );
+            } elseif ( is_single() ) {
             
-                if ( $taxonomies ) {
-                    $terms = get_the_terms( get_the_ID(), $taxonomy );
-            
-                    //keeps only the first term (categ)
-                    $term = reset( $terms );
-                    if ( 0 != $term->parent ) {
-            
-                        // Get term ancestors info
-                        $ancestors = array_reverse( get_ancestors( $term->term_id, $taxonomy ) );
-                        // Print loop term ancestors
-                        foreach ( $ancestors as $ancestor ) :
-                            $pan_term           = get_term( $ancestor, $taxonomy );
-                            $breadcrumb_array[] = array(
-                                'name'             => $pan_term->name,
-                                'id'               => '',
-                                'url'              => get_term_link( $ancestor, $taxonomy ),
-                                'class'            => '',
-                            );
-                        endforeach;
-                    } // if ( 0 != $term->parent ) {
-                    $term_url           = get_term_link( $term->term_id, $taxonomy );
-                    $breadcrumb_array[] = array(
-                        'name'             => $term->name,
-                        'id'               => '',
-                        'url'              => $term_url,
-                        'class'            => '',
-                    );
-                } // if ( $taxonomies ) {
-            } // if ( $post_type_info['slug'] == 'post' ) {
+                /* Single
+                /*-------------------------------*/
+                
+                // Case of post
+                
+                if ( $post_type_info['slug'] == 'post' ) {
+                    $category = get_the_category();
+                    // get parent category info
+                    $parents = array_reverse( get_ancestors( $category[0]->term_id, 'category', 'taxonomy' ) );
+                    array_push( $parents, $category[0]->term_id );
+                    foreach ( $parents as $parent_term_id ) {
+                        $parent_obj         = get_term( $parent_term_id, 'category' );
+                        $term_url           = get_term_link( $parent_obj->term_id, $parent_obj->taxonomy );
+                        $breadcrumb_array[] = array(
+                            'name'             => $parent_obj->name,
+                            'id'               => '',
+                            'url'              => $term_url,
+                            'class'            => '',
+                        );
+                    }
+                
+                // Case of custom post type
+                
+                } else {
+
+                    // Taxonomy of Single or Page.
+                    $get_taxonomies = get_the_taxonomies();
+                    $taxonomies     = array();
+
+                    // 一旦タクソノミーの文字列配列に変換しないと色々と面倒.
+                    foreach ( $get_taxonomies as $key => $value ) {
+                        $taxonomies[] = $key;
+                    }
+
+                    // 除外するタクソノミーの文字列配列.
+                    $exclusion = array();
+                    $exclusion = apply_filters( 'vk_breadcrumb_taxonomies_exludion', $exclusion );
+        
+                    // タクソノミーの差分を採用.
+                    if ( $exclusion ){
+                        $taxonomies         = array_diff( $taxonomies, $exclusion );
+                    }
+                
+                    if ( $taxonomies ) {
+
+                        foreach ( $taxonomies as $key ) {
+                            $taxonomy = $key;
+                            break;
+                        }
+
+                        $terms = get_the_terms( get_the_ID(), $taxonomy );
+                
+                        //keeps only the first term (categ)
+                        $term = reset( $terms );
+                        if ( 0 != $term->parent ) {
+                
+                            // Get term ancestors info
+                            $ancestors = array_reverse( get_ancestors( $term->term_id, $taxonomy ) );
+                            // Print loop term ancestors
+                            foreach ( $ancestors as $ancestor ) {
+                                $pan_term           = get_term( $ancestor, $taxonomy );
+                                $breadcrumb_array[] = array(
+                                    'name'             => $pan_term->name,
+                                    'id'               => '',
+                                    'url'              => get_term_link( $ancestor, $taxonomy ),
+                                    'class'            => '',
+                                );
+                            }
+                        } // if ( 0 != $term->parent ) {
+                        $term_url           = get_term_link( $term->term_id, $taxonomy );
+                        $breadcrumb_array[] = array(
+                            'name'             => $term->name,
+                            'id'               => '',
+                            'url'              => $term_url,
+                            'class'            => '',
+                        );
+                    } // if ( $taxonomies ) {
+                } // if ( $post_type_info['slug'] == 'post' ) {
 
                 $breadcrumb_array[] = array(
                     'name'             => get_the_title(),
@@ -350,6 +354,50 @@ if ( ! class_exists( 'VK_Breadcrumb' ) ) {
                 <!-- [ /#' . esc_attr( $breadcrumb_options['class_outer'] ) . ' ] -->
                 ';
                 $breadcrumb_html  = apply_filters( 'vk_breadcrumb_html', $breadcrumb_html );
+
+                $allowed_html = array(
+                    'div'  => array(
+                        'id'        => array(),
+                        'class'     => array(),
+                        'itemprop'  => array(),
+                        'itemscope' => array(),
+                        'itemtype'  => array(),
+                    ),
+                    'ol'   => array(
+                        'id'        => array(),
+                        'class'     => array(),
+                        'itemprop'  => array(),
+                        'itemscope' => array(),
+                        'itemtype'  => array(),
+                    ),
+                    'li'   => array(
+                        'id'        => array(),
+                        'class'     => array(),
+                        'itemprop'  => array(),
+                        'itemscope' => array(),
+                        'itemtype'  => array(),
+                    ),
+                    'a'    => array(
+                        'id'       => array(),
+                        'class'    => array(),
+                        'href'     => array(),
+                        'target'   => array(),
+                        'itemprop' => array(),
+                    ),
+                    'span' => array(
+                        'id'        => array(),
+                        'class'     => array(),
+                        'itemprop'  => array(),
+                        'itemscope' => array(),
+                        'itemtype'  => array(),
+                    ),
+                    'i'    => array(
+                        'id'    => array(),
+                        'class' => array(),
+                    ),
+                );
+
+                $breadcrumb_html  = wp_kses( $breadcrumb_html, $allowed_html );
                 echo $breadcrumb_html;
 
         }
