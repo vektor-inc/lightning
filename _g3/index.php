682c7fa62869bdbeead0ed29d2551903ff277180
kurudrive <kurudrive@gmail.com>
2020-12-22 15:19:07 +0900

    [ G3 ] index.php フレーム調整

--- _g3/index.php
+++ _g3/index.php
@@ -1,11 +1,11 @@
 <?php lightning_get_template_part( 'header' ); ?>
 
 <?php 
-if ( lightning_is_site_header() ){
-    do_action( 'lightning_header_before' );
-    lightning_get_template_part( 'template-parts/header' );
-    do_action( 'lightning_header_after' );
-} 
+do_action( 'lightning_site-header_before' );
+if ( apply_filters( 'lightning_is_site-header', true ) ){
+    lightning_get_template_part( 'template-parts/site-header' );
+}
+do_action( 'lightning_site-header_after' );
 ?>
 
 <div class="<?php lightning_the_class_name( 'site-body' ); ?>">
@@ -13,17 +13,21 @@ if ( lightning_is_site_header() ){
 
     <div class="<?php lightning_the_class_name( 'site-body__container' ); ?> container">
 
-        <?php if ( lightning_is_page_header() ){
-            do_action( 'lightning_page-header_before' );
+        <?php 
+        do_action( 'lightning_page-header_before' );
+        if ( apply_filters( 'lightning_is_page-header', true ) ){
             lightning_get_template_part( 'template-parts/page-header' );
-            do_action( 'lightning_page-header_after' );
-        } ?>
+        }
+        do_action( 'lightning_page-header_after' );
+        ?>
 
-        <?php if ( lightning_is_breadcrumb() ){
-            do_action( 'lightning_breadcrumb_before' );
+        <?php 
+        do_action( 'lightning_breadcrumb_before' );
+        if ( apply_filters( 'lightning_is_breadcrumb', true ) ){
             VK_Breadcrumb::the_breadcrumb();
-            do_action( 'lightning_breadcrumb_after' );
-        } ?>
+        }
+        do_action( 'lightning_breadcrumb_after' );
+        ?>
 
         <div class="<?php lightning_the_class_name( 'main-section' ); ?>" id="main" role="main">
             <?php do_action( 'lightning_main-section_prepend' ); ?>
@@ -31,9 +35,13 @@ if ( lightning_is_site_header() ){
             <?php do_action( 'lightning_main-section_append' ); ?>
         </div><!-- [ /.main-section ] -->
 
-        <?php if ( lightning_is_subsection_display() ){ ?>
-            <?php get_sidebar( get_post_type() ); ?>
-        <?php } ?>
+        <?php 
+        do_action( 'lightning_sub-section_before' );
+        if ( apply_filters( 'lightning_is_sub-section', true ) ){ 
+            get_sidebar( get_post_type() );
+        }
+        do_action( 'lightning_sub-section_after' );
+        ?>
 
     </div><!-- [ /.site-body__container ] -->
 
@@ -41,10 +49,11 @@ if ( lightning_is_site_header() ){
 </div><!-- [ /.site-body ] -->
 
 <?php 
-if ( lightning_is_site_header() ){
-    do_action( 'lightning_header_before' );
-    lightning_get_template_part( 'template-parts/header' );
-    do_action( 'lightning_header_after' );
-} 
+do_action( 'lightning_site-footer_before' );
+if ( apply_filters( 'lightning_is_site-footer', true ) ) {
+    lightning_get_template_part( 'template-parts/site-footer' );
+}
+do_action( 'lightning_site-footer_after' );
 ?>
+
 <?php lightning_get_template_part( 'footer' ); ?>
