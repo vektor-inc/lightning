a5babaf1186c3c716dd428dfe5fe3c5c6255656d
kurudrive <kurudrive@gmail.com>
2021-02-06 17:57:15 +0900

    [ G3 ] スラッグのテンプレートを読み込むように

--- _g3/template-parts/main-singular.php
+++ _g3/template-parts/main-singular.php
@@ -4,8 +4,14 @@ if ( apply_filters( 'is_lightning_extend_single', false ) ) :
 else :
 	if ( have_posts() ) :
 		while ( have_posts() ) :
-            the_post();
+			the_post();
+			$template = 'template-parts/entry-' . esc_attr( $post->post_name ).'.php';
+			$return = locate_template( $template );
+			if ( $return && $post->post_name != get_post_type() ){
+				locate_template( $template, true );
+			} else {
 				lightning_get_template_part( 'template-parts/entry', get_post_type() );
+			}
 		endwhile;
 	endif; // if ( have_posts() ) :
 endif; // if ( apply_filters( 'is_lightning_extend_single', false ) ) :
