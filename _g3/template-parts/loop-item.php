4798c133866453be8b1508867096b41640c75755
kurudrive <kurudrive@gmail.com>
2021-02-06 20:10:16 +0900

    [ G3 ] #512 投稿1件分は vk compo でいいっしょ

--- _g3/template-parts/loop-item.php
+++ _g3/template-parts/loop-item.php
@@ -1,3 +1,21 @@
 <?php
-/* This file is used to fallback when post type template file was don't exist. */
-lightning_get_template_part( 'template-parts/loop-item-layout', 'media' );
+$options = array(
+	'layout'                     => 'media', // card , card-horizontal , media
+	'display_image'              => true,
+	'display_image_overlay_term' => true,
+	'display_excerpt'            => true,
+	'display_date'               => true,
+	'display_new'                => true,
+	'display_btn'                => true,
+	'image_default_url'          => false,
+	'overlay'                    => false,
+	'btn_text'                   => __( 'Read more', 'lightning' ),
+	'btn_align'                  => 'text-right',
+	'new_text'                   => __( 'New!!', 'lightning' ),
+	'new_date'                   => 7,
+	'class_outer'                => 'vk_post-col-sm-12 vk_post-col-lg-12',
+	'class_title'                => '',
+	'body_prepend'               => '',
+	'body_append'                => '',
+);
+wp_kses_post( VK_Component_Posts::the_view( $post, $options ) );
