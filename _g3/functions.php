1828091dd54722a085d7c0301f6200316688948e
kurudrive <kurudrive@gmail.com>
2021-02-20 22:04:11 +0900

    [ G3 ] グラデと余白サポートを追加

--- _g3/functions.php
+++ _g3/functions.php
@@ -61,6 +61,39 @@ function lightning_theme_setup() {
 		)
 	) );
 
+	add_theme_support(
+		'editor-gradient-presets',
+		array(
+			array(
+				'name'     => esc_attr__( 'Vivid cyan blue to vivid purple', 'themeLangDomain' ),
+				'gradient' => 'linear-gradient(135deg,rgba(6,147,227,1) 0%,rgb(155,81,224) 100%)',
+				'slug'     => 'vivid-cyan-blue-to-vivid-purple'
+			),
+			array(
+				'name'     => esc_attr__( 'Vivid green cyan to vivid cyan blue', 'themeLangDomain' ),
+				'gradient' => 'linear-gradient(135deg,rgba(0,208,132,1) 0%,rgba(6,147,227,1) 100%)',
+				'slug'     =>  'vivid-green-cyan-to-vivid-cyan-blue',
+			),
+			array(
+				'name'     => esc_attr__( 'Light green cyan to vivid green cyan', 'themeLangDomain' ),
+				'gradient' => 'linear-gradient(135deg,rgb(122,220,180) 0%,rgb(0,208,130) 100%)',
+				'slug'     => 'light-green-cyan-to-vivid-green-cyan',
+			),
+			array(
+				'name'     => esc_attr__( 'Luminous vivid amber to luminous vivid orange', 'themeLangDomain' ),
+				'gradient' => 'linear-gradient(135deg,rgba(252,185,0,1) 0%,rgba(255,105,0,1) 100%)',
+				'slug'     => 'luminous-vivid-amber-to-luminous-vivid-orange',
+			),
+			array(
+				'name'     => esc_attr__( 'Luminous vivid orange to vivid red', 'themeLangDomain' ),
+				'gradient' => 'linear-gradient(135deg,rgba(255,105,0,1) 0%,rgb(207,46,46) 100%)',
+				'slug'     => 'luminous-vivid-orange-to-vivid-red',
+			),
+		)
+	);
+
+	add_theme_support('custom-spacing');
+
 
 	/*
 	  cope with page excerpt
