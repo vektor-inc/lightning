f5d5988429ee5d775ef1130445f93457fbe8ebf5
kurudrive <kurudrive@gmail.com>
2021-03-05 20:02:41 +0900

    G3 bbPressのパンクス改変が旧タイプなので一旦コメントアウト

--- _g3/plugin-support/bbpress/functions-bbpress.php
+++ _g3/plugin-support/bbpress/functions-bbpress.php
@@ -28,47 +28,47 @@ add_action( 'wp_enqueue_scripts', 'lightning_bbp_load_css' );
 /*
   フォーラムのパンくずリスト書き換え
 /*-------------------------------------------*/
-add_filter(
-	'lightning_panListHtml',
-	function( $panListHtml ) {
-		if ( function_exists( 'bbp_get_forum_post_type' ) ) {
-			$postType = lightning_get_post_type();
-			if ( $postType['slug'] == 'topic' ) {
+// add_filter(
+// 	'lightning_panListHtml',
+// 	function( $panListHtml ) {
+// 		if ( function_exists( 'bbp_get_forum_post_type' ) ) {
+// 			$postType = lightning_get_post_type();
+// 			if ( $postType['slug'] == 'topic' ) {
 
-				// Microdata
-				// http://schema.org/BreadcrumbList
-				/*-------------------------------------------*/
-				$microdata_li = ' itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"';
+// 				// Microdata
+// 				// http://schema.org/BreadcrumbList
+// 				/*-------------------------------------------*/
+// 				$microdata_li = ' itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"';
 
-				$before_html = '<!-- [ .breadSection ] -->
-<div class="section breadSection">
-<div class="container">
-<div class="row">
-<ol class="breadcrumb" itemtype="http://schema.org/BreadcrumbList">';
+// 				$before_html = '<!-- [ .breadSection ] -->
+// <div class="section breadSection">
+// <div class="container">
+// <div class="row">
+// <ol class="breadcrumb" itemtype="http://schema.org/BreadcrumbList">';
 
-				$after_html = '</ol>
-</div>
-</div>
-</div>
-<!-- [ /.breadSection ] -->';
+// 				$after_html = '</ol>
+// </div>
+// </div>
+// </div>
+// <!-- [ /.breadSection ] -->';
 
-				$args        = array(
-					// HTML
-					'before'         => $before_html,
-					'after'          => $after_html,
-					'sep'            => '',
-					'crumb_before'   => '<li' . $microdata_li . '><span>',
-					'crumb_after'    => '</span></li>',
-					'home_text'      => '<i class="fa fa-home"></i> HOME',
-					'current_before' => '',
-					'current_after'  => '',
-				);
-				$panListHtml = bbp_get_breadcrumb( $args );
-			}
-		}
-		return $panListHtml;
-	}
-);
+// 				$args        = array(
+// 					// HTML
+// 					'before'         => $before_html,
+// 					'after'          => $after_html,
+// 					'sep'            => '',
+// 					'crumb_before'   => '<li' . $microdata_li . '><span>',
+// 					'crumb_after'    => '</span></li>',
+// 					'home_text'      => '<i class="fa fa-home"></i> HOME',
+// 					'current_before' => '',
+// 					'current_after'  => '',
+// 				);
+// 				$panListHtml = bbp_get_breadcrumb( $args );
+// 			}
+// 		}
+// 		return $panListHtml;
+// 	}
+// );
 
 /*
   トピックの内容の前にトピックタイトル追加
