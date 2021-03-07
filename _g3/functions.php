65efd5c88ab8729c290474cecc597e2e8266dc11
kurudrive <kurudrive@gmail.com>
2021-03-08 01:00:02 +0900

    G3 G2から移植しきれてない処理確認・調整 #564

--- _g3/functions.php
+++ _g3/functions.php
@@ -241,6 +241,25 @@ function lightning_load_fonts(){
 }
 add_action( 'wp_head', 'lightning_load_fonts' );
 
+
+/*
+  Year Artchive list 'year' and count insert to inner </a>
+/*-------------------------------------------*/
+function lightning_archives_link( $html ) {
+	return preg_replace( '@</a>(.+?)</li>@', '\1</a></li>', $html );
+}
+add_filter( 'get_archives_link', 'lightning_archives_link' );
+
+/*
+  Category list count insert to inner </a>
+/*-------------------------------------------*/
+function lightning_list_categories( $output, $args ) {
+	$output = preg_replace( '/<\/a>\s*\((\d+)\)/', ' ($1)</a>', $output );
+	return $output;
+}
+add_filter( 'wp_list_categories', 'lightning_list_categories', 10, 2 );
+
+
 /*
   Load embed card css
 /*-------------------------------------------*/
