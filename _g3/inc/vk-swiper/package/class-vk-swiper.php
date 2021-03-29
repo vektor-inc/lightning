6a2b2d025dfd57356b73ca8df609a207d4a4d74e
kurudrive <kurudrive@gmail.com>
2021-03-30 01:24:33 +0900

    G3 swiperのハンドル名を一時的に変更 ★ 要塞調整

--- _g3/inc/vk-swiper/package/class-vk-swiper.php
+++ _g3/inc/vk-swiper/package/class-vk-swiper.php
@@ -27,7 +27,7 @@ if ( ! class_exists( 'VK_Swiper' ) ) {
 		public static function load_swiper() {
 			global $vk_swiper_url;
 			wp_enqueue_style( 'swiper-style', $vk_swiper_url . '/assets/css/swiper.min.css', array(), SWIPER_VERSION );
-			wp_enqueue_script( 'swiper-js', $vk_swiper_url . '/assets/js/swiper.min.js', array(), SWIPER_VERSION, true );
+			wp_enqueue_script( 'vk-blocks-swiper', $vk_swiper_url . '/assets/js/swiper.min.js', array(), SWIPER_VERSION, true );
 		}
 	}
 	VK_Swiper::init();
