be34e03dccc327aa0884fcaf20874d810f034c36
drill-lancer <rickaddison7634@gmail.com>
2021-04-05 12:36:05 +0900

    fix seiper

--- _g3/inc/vk-swiper/config.php
+++ _g3/inc/vk-swiper/config.php
@@ -11,15 +11,15 @@
  */
 
 $options = get_option( 'lightning_theme_options' );
-if ( isset( $options['top_slide_display'] ) && 'hide' === $options['top_slide_display'] ) {
+if ( ! empty( $options['top_slide_display'] ) && 'hide' === $options['top_slide_display'] ) {
 	return;
 }
 
 if ( ! class_exists( 'VK_Swiper' ) ) {
 	global $vk_swiper_url;
 	global $vk_swiper_path;
-	$vk_swiper_url = get_parent_theme_file_uri( 'inc/vk-swiper/package' );
-	$vk_swiper_path = get_parent_theme_file_path( 'inc/vk-swiper/package' );
+	$vk_swiper_url = get_parent_theme_file_uri( 'inc/vk-swiper/package/' );
+	$vk_swiper_path = get_parent_theme_file_path( '/inc/vk-swiper/package/' );
 	require_once dirname( __FILE__ ) . '/package/class-vk-swiper.php';
 }
 
