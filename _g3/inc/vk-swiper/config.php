037cf12cfaf6619be7a75f4824d68b39d203afc2
drill-lancer <rickaddison7634@gmail.com>
2021-04-05 15:40:30 +0900

    change path

--- _g3/inc/vk-swiper/config.php
+++ _g3/inc/vk-swiper/config.php
@@ -19,7 +19,7 @@ if ( ! class_exists( 'VK_Swiper' ) ) {
 	global $vk_swiper_url;
 	global $vk_swiper_path;
 	$vk_swiper_url = get_parent_theme_file_uri( 'inc/vk-swiper/package/' );
-	$vk_swiper_path = get_parent_theme_file_path( '/inc/vk-swiper/package/' );
+	$vk_swiper_path = get_parent_theme_file_path( 'inc/vk-swiper/package/' );
 	require_once dirname( __FILE__ ) . '/package/class-vk-swiper.php';
 }
 
