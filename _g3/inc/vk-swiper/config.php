f8d33abf7dec508f75bf0d814b3bf0b149612a88
drill-lancer <rickaddison7634@gmail.com>
2021-04-05 10:32:42 +0900

    fix read condition

--- _g3/inc/vk-swiper/config.php
+++ _g3/inc/vk-swiper/config.php
@@ -5,22 +5,21 @@
  * @package Lightning G3
  */
 
+/**
+ * Swiper is used only on the front page.
+ * If you do not use the slider on the front page, Swiper will not be loaded.
+ *
+ *
+ */
+$options = get_option( 'vk_advanced_slider_option' );
+$default = vk_advanced_slider_default_options();
+$options = wp_parse_args( $options, $default );
+
+if ( 'hide' !== $options['top_slide_display'] ) {
+	return;
+}
+
 if ( ! class_exists( 'VK_Swiper' ) ) {
-
-	/**
-	 * Swiper is used only on the front page.
-	 * If you do not use the slider on the front page, Swiper will not be loaded.
-	 *  */ 
-	/*
-	Because using on VK Blocks Pro, This code will comment out.
-
-
-	$options = get_option( 'lightning_theme_options' );
-	if ( 'hide' === isset( $options['top_slide_display'] ) ){
-		return;
-	}
-	*/
-
 	global $vk_swiper_url;
 	$vk_swiper_url = get_parent_theme_file_uri( 'inc/vk-swiper/package' );
 	require_once dirname( __FILE__ ) . '/package/class-vk-swiper.php';
