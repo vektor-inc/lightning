9811fd0f32a072f1ac88a2b7062503b7ab1d406d
drill-lancer <rickaddison7634@gmail.com>
2021-04-05 12:16:02 +0900

    tree-shaking

--- _g3/inc/vk-swiper/config.php
+++ _g3/inc/vk-swiper/config.php
@@ -16,8 +16,6 @@ if ( isset( $options['top_slide_display'] ) && 'hide' === $options['top_slide_di
 }
 
 if ( ! class_exists( 'VK_Swiper' ) ) {
-	global $vk_swiper_url;
-	$vk_swiper_url = get_parent_theme_file_uri( 'inc/vk-swiper/package' );
 	require_once dirname( __FILE__ ) . '/package/class-vk-swiper.php';
 }
 
--- _g3/inc/vk-swiper/package/class-vk-swiper.php
+++ _g3/inc/vk-swiper/package/class-vk-swiper.php
@@ -5,6 +5,15 @@
  * @package Katawara
  */
 
+/*
+このファイルの元ファイルは
+https://github.com/vektor-inc/vektor-wp-libraries
+にあります。
+修正の際は上記リポジトリのデータを修正してください。
+編集権限を持っていない方で何か修正要望などありましたら
+各プラグインのリポジトリにプルリクエストで結構です。
+*/
+
 if ( ! class_exists( 'VK_Swiper' ) ) {
 
 	// Set version number.
@@ -19,16 +28,27 @@ if ( ! class_exists( 'VK_Swiper' ) ) {
 		 */
 		public static function init() {
 			add_action( 'wp_enqueue_scripts', array( __CLASS__, 'load_swiper' ) );
+			add_filter( 'vk_css_tree_shaking_array', array( __CLASS__, 'css_tree_shaking_array' ), 11 );
 		}
 
 		/**
 		 * Load Swiper
 		 */
 		public static function load_swiper() {
-			global $vk_swiper_url;
-			wp_enqueue_style( 'vk-swiper-style', $vk_swiper_url . '/assets/css/swiper.min.css', array(), SWIPER_VERSION );
-			wp_enqueue_script( 'vk-swiper-script', $vk_swiper_url . '/assets/js/swiper.min.js', array(), SWIPER_VERSION, true );
+			wp_enqueue_style( 'vk-swiper-style', plugin_dir_url( __FILE__ ) . 'assets/css/swiper.min.css', array(), SWIPER_VERSION );
+			wp_enqueue_script( 'vk-swiper-script', plugin_dir_url( __FILE__ ) . 'assets/js/swiper.min.js', array(), SWIPER_VERSION, true );
+		}
+
+		public static function css_tree_shaking_array( $vk_css_tree_shaking_array ){
+			$vk_css_tree_shaking_array[] = array(
+				'id'      => 'vk-swiper-style',
+				'url'     => plugin_dir_url( __FILE__ ) . 'assets/css/swiper.min.css',
+				'path'    => plugin_dir_path( __FILE__ ) . 'assets/css/swiper.min.css',
+				'version' => SWIPER_VERSION,
+			);
+			return $vk_css_tree_shaking_array;
 		}
 	}
 	VK_Swiper::init();
 }
+
