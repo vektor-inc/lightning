7f94aa4b1ab8eb236ec8e295052d600843c55015
kurudrive <kurudrive@gmail.com>
2021-04-25 01:20:42 +0900

    G3 bbPress対応（途中）

--- _g3/plugin-support/bbpress/functions-bbpress.php
+++ _g3/plugin-support/bbpress/functions-bbpress.php
@@ -26,57 +26,33 @@ function lightning_bbp_load_css() {
 add_action( 'wp_enqueue_scripts', 'lightning_bbp_load_css' );
 
 /*
-  フォーラムのパンくずリスト書き換え
-/*-------------------------------------------*/
-// add_filter(
-// 	'lightning_panListHtml',
-// 	function( $panListHtml ) {
-// 		if ( function_exists( 'bbp_get_forum_post_type' ) ) {
-// 			$postType = lightning_get_post_type();
-// 			if ( $postType['slug'] == 'topic' ) {
-
-// 				// Microdata
-// 				// http://schema.org/BreadcrumbList
-// 				/*-------------------------------------------*/
-// 				$microdata_li = ' itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"';
-
-// 				$before_html = '<!-- [ .breadSection ] -->
-// <div class="section breadSection">
-// <div class="container">
-// <div class="row">
-// <ol class="breadcrumb" itemtype="http://schema.org/BreadcrumbList">';
-
-// 				$after_html = '</ol>
-// </div>
-// </div>
-// </div>
-// <!-- [ /.breadSection ] -->';
-
-// 				$args        = array(
-// 					// HTML
-// 					'before'         => $before_html,
-// 					'after'          => $after_html,
-// 					'sep'            => '',
-// 					'crumb_before'   => '<li' . $microdata_li . '><span>',
-// 					'crumb_after'    => '</span></li>',
-// 					'home_text'      => '<i class="fa fa-home"></i> HOME',
-// 					'current_before' => '',
-// 					'current_after'  => '',
-// 				);
-// 				$panListHtml = bbp_get_breadcrumb( $args );
-// 			}
-// 		}
-// 		return $panListHtml;
-// 	}
-// );
-
-/*
   トピックの内容の前にトピックタイトル追加
 /*-------------------------------------------*/
 function lightning_bbp_add_topic_title() {
 	$skin = get_option( 'lightning_design_skin' );
-	if ( $skin != 'Variety' ) {
-		echo '<div><h2>' . get_the_title() . '</h2></div>';
-	}
+	echo '<div><h2>' . get_the_title() . '</h2></div>';
 }
 add_action( 'bbp_template_before_single_topic', 'lightning_bbp_add_topic_title' );
+
+/**
+ * ユーザープロフィールページのページヘッダー
+ */
+function lightning_bbp_page_header_title( $page_title ) {
+	if ( bbp_is_single_user() ) {
+		$page_title = __( 'User Profile', 'lightning' );
+	}
+	return $page_title;
+}
+add_filter( 'lightning_pageTitCustom', 'lightning_bbp_page_header_title' );
+
+
+/**
+ * ユーザープロフィールページの投稿タイプ
+ */
+function lightning_bbp_get_post_type( $post_type ) {
+	if ( bbp_is_single_user() ) {
+		$post_type['slug'] = 'bbp_user';
+	}
+	return $post_type;
+}
+add_filter( 'vk_get_post_type_info', 'lightning_bbp_get_post_type' );
