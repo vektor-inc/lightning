f970d5ad834e65fcbc4c30885b021389eb0d9c53
kurudrive <kurudrive@gmail.com>
2021-05-10 23:27:07 +0900

    phpcs

--- _g3/inc/template-tags.php
+++ _g3/inc/template-tags.php
@@ -2,72 +2,72 @@
 
 function lightning_get_class_names( $position = '' ) {
 
-    $class_names = array(
-        'site-header' => array( 
-            'site-header',
-            'site-header--layout--nav-float',
-        ),
-        'global-nav' => array( 
-            'global-nav',
-            'global-nav--layout--float-right',
-        ),
-        'site-body' => array( 'site-body' ),
-        'main-section' => array( 'main-section' ),
-        'sub-section' => array( 'sub-section' ),
-    );
+	$class_names = array(
+		'site-header'  => array(
+			'site-header',
+			'site-header--layout--nav-float',
+		),
+		'global-nav'   => array(
+			'global-nav',
+			'global-nav--layout--float-right',
+		),
+		'site-body'    => array( 'site-body' ),
+		'main-section' => array( 'main-section' ),
+		'sub-section'  => array( 'sub-section' ),
+	);
 
-    if ( empty( $class_names[$position] ) ){
-        $class_names[$position][] = $position;
-    }
+	if ( empty( $class_names[ $position ] ) ) {
+		$class_names[ $position ][] = $position;
+	}
 
-    /**
-     * カラムでの配列操作
-     * （本来はカラムコントローラーからフックで処理するのが望ましい）
-     */
-    if ( lightning_is_layout_onecolumn() && lightning_is_subsection() ) {
+	/**
+	 * カラムでの配列操作
+	 * （本来はカラムコントローラーからフックで処理するのが望ましい）
+	 */
+	if ( lightning_is_layout_onecolumn() && lightning_is_subsection() ) {
 
-        $class_names['main-section'][] = 'main-section--margin-bottom--on';
+		$class_names['main-section'][] = 'main-section--margin-bottom--on';
 
-    } elseif ( ! lightning_is_layout_onecolumn() ) {
+	} elseif ( ! lightning_is_layout_onecolumn() ) {
 
-        $class_names['main-section'][] = 'main-section--col--two';
-        $class_names['sub-section'][] = 'sub-section--col--two';
-        // 2 column
-        $options = get_option( 'lightning_theme_options' );
-        // sidebar-position
-        if ( isset( $options['sidebar_position'] ) && $options['sidebar_position'] === 'left' ) {
-            $class_names['main-section'][] = 'main-section--pos--right';
-            $class_names['sub-section'][] = 'sub-section--pos--left';
-        }
-    }
+		$class_names['main-section'][] = 'main-section--col--two';
+		$class_names['sub-section'][]  = 'sub-section--col--two';
+		// 2 column
+		$options = get_option( 'lightning_theme_options' );
+		// sidebar-position
+		if ( isset( $options['sidebar_position'] ) && $options['sidebar_position'] === 'left' ) {
+			$class_names['main-section'][] = 'main-section--pos--right';
+			$class_names['sub-section'][]  = 'sub-section--pos--left';
+		}
+	}
 
-    if ( lightning_is_site_body_padding_off() ) {
-        $class_names['site-body'][] = 'site-body--padding-vertical--off';
-        $class_names['main-section'][] = 'main-section--margin-vertical--off';
-    }
+	if ( lightning_is_site_body_padding_off() ) {
+		$class_names['site-body'][]    = 'site-body--padding-vertical--off';
+		$class_names['main-section'][] = 'main-section--margin-vertical--off';
+	}
 
-    $class_names = apply_filters( "lightning_get_class_names", $class_names );
-    return $class_names;
+	$class_names = apply_filters( 'lightning_get_class_names', $class_names );
+	return $class_names;
 }
 
-function lightning_get_the_class_name( $position = '' ){
-    $class_names = lightning_get_class_names( $position );
+function lightning_get_the_class_name( $position = '' ) {
+	$class_names = lightning_get_class_names( $position );
 
-    // すべてのクラス名から単一のクラスを代入
-    if ( $position && empty( $class_names[$position] ) ){
-        $class_name = $position;    
-    } else {
-        $class_name = implode( " ",  $class_names[$position] );
-    }
+	// すべてのクラス名から単一のクラスを代入
+	if ( $position && empty( $class_names[ $position ] ) ) {
+		$class_name = $position;
+	} else {
+		$class_name = implode( ' ', $class_names[ $position ] );
+	}
 
-    // 元の配列（lightning_get_class_names）はフック操作が少し難しいので単純に書き換えたい人用
-    $class_name = esc_attr( apply_filters( 'lightning_get_the_class_name_' . $position, $class_name ) );
+	// 元の配列（lightning_get_class_names）はフック操作が少し難しいので単純に書き換えたい人用
+	$class_name = esc_attr( apply_filters( 'lightning_get_the_class_name_' . $position, $class_name ) );
 
-    return esc_attr( apply_filters( "lightning_get_the_class_name", $class_name, $position ) );
+	return esc_attr( apply_filters( 'lightning_get_the_class_name', $class_name, $position ) );
 }
 
-function lightning_the_class_name( $position = '' ){
-    echo lightning_get_the_class_name( $position );
+function lightning_the_class_name( $position = '' ) {
+	echo lightning_get_the_class_name( $position );
 }
 
 /*
@@ -75,8 +75,8 @@ function lightning_the_class_name( $position = '' ){
 /*-------------------------------------------*/
 function lightning_get_theme_options_default() {
 	$theme_options_default = array(
-		'layout'       => array(
-			'front-page'	=> 'col-one-no-subsection',
+		'layout' => array(
+			'front-page' => 'col-one-no-subsection',
 		),
 	);
 	return $theme_options_default;
@@ -183,18 +183,18 @@ function lightning_get_prefix_customize_panel() {
 	return $prefix_customize_panel;
 }
 
-function lightning_get_entry_meta( $options = array() ){
+function lightning_get_entry_meta( $options = array() ) {
 
-	$defaults = [
-		'published' => true,
-		'updated' => true,
-		'author_name' => true,
+	$defaults = array(
+		'published'    => true,
+		'updated'      => true,
+		'author_name'  => true,
 		'author_image' => true,
-		'class_outer' => 'entry-meta',
-	];
+		'class_outer'  => 'entry-meta',
+	);
 
 	$option = apply_filters( 'lightning_get_entry_meta_options', wp_parse_args( $options, $defaults ) );
-	
+
 	if ( $option['published'] || $option['updated'] || $option['author_name'] || $option['author_image'] ) {
 		$html = '<div class="' . $option['class_outer'] . '">';
 
@@ -212,58 +212,58 @@ function lightning_get_entry_meta( $options = array() ){
 			<span class="updated">' . get_the_modified_date( '' ) . '</span>
 			</span>';
 		}
-	
+
 		if ( $option['author_name'] || $option['author_image'] ) {
 			// Post author
 			// For post type where author does not exist
 			$author = get_the_author();
 			if ( $author ) {
 				$meta_hidden_author = ( ! empty( $options['postAuthor_hidden'] ) ) ? ' entry-meta_hidden' : '';
-			
+
 				$html .= '<span class="entry-meta-item entry-meta-item-author' . $meta_hidden_author . '">
 				<span class="vcard author" itemprop="author">';
 
 				if ( $option['author_image'] ) {
 					$html .= '<span class="entry-meta-item-author-image">';
-		
+
 					// VK Post Author Display の画像を取得
 					$profile_image_id = get_the_author_meta( 'user_profile_image' );
 					if ( $profile_image_id ) {
 						$profile_image_src = wp_get_attachment_image_src( $profile_image_id, 'thumbnail' );
-						$html .= '<img src="' . $profile_image_src[0] . '" alt="' . esc_attr( $author ) . '" />';
+						$html             .= '<img src="' . $profile_image_src[0] . '" alt="' . esc_attr( $author ) . '" />';
 					} else {
 						$html .= get_avatar( get_the_author_meta( 'email' ), 30 );
 					}
-		
+
 					$html .= '</span>';
 				}
 
 				if ( $option['author_name'] ) {
 					$html .= '<span class="fn" itemprop="name">' . esc_html( $author ) . '</span>';
 				}
-				
+
 				$html .= '</span></span>';
-			
+
 			} // if ( $author ) {
 		}
 
 		$html .= '</div>';
 	}
 
-	return apply_filters( 'lightning_get_entry_meta',  $html );
+	return apply_filters( 'lightning_get_entry_meta', $html );
 }
 
-function lightning_the_entry_meta( $options = array() ){
+function lightning_the_entry_meta( $options = array() ) {
 	echo wp_kses_post( lightning_get_entry_meta() );
 }
 
 
-function lightning_is_woo_page(){
-	if ( ! class_exists( 'woocommerce' ) ){
+function lightning_is_woo_page() {
+	if ( ! class_exists( 'woocommerce' ) ) {
 		return false;
 	}
 	$post_type_info = VK_Helpers::get_post_type_info();
-	if ( isset( $post_type_info['slug'] ) && 'product' === $post_type_info['slug'] ){
+	if ( isset( $post_type_info['slug'] ) && 'product' === $post_type_info['slug'] ) {
 		return true;
 	}
 	return false;
