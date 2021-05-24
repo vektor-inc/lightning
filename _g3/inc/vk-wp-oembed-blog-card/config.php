b9a506d8e8ddb10a98e657560a3c42eed21c2347
kurudrive <kurudrive@gmail.com>
2021-05-25 01:07:28 +0900

    ブログカード要塞調整につき一旦変更削除

--- _g3/inc/vk-wp-oembed-blog-card/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*-------------------------------------------*/
-/*  Load modules
-/*-------------------------------------------*/
-if ( ! class_exists( 'VK_WP_Oembed_Blog_Card' ) ) {
-  require dirname( __FILE__ ) . '/package/class-vk-wp-oembed-blog-card.php';
-}
