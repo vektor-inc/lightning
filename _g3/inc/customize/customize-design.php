345f7d50756606c23b7e4a78e3bada8252b83760
kurudrive <kurudrive@gmail.com>
2021-05-24 16:45:51 +0900

    doc: コメント追加のみ

--- _g3/inc/customize/customize-design.php
+++ _g3/inc/customize/customize-design.php
@@ -137,4 +137,5 @@ function lightning_add_common_dynamic_css_to_editor() {
 	$dynamic_css = lightning_get_common_inline_css();
 	wp_add_inline_style( 'lightning-common-editor-gutenberg', $dynamic_css, 11 );
 }
+// 11 指定が無いと先に読み込んでしまって効かない
 add_action( 'enqueue_block_editor_assets', 'lightning_add_common_dynamic_css_to_editor', 11 );
