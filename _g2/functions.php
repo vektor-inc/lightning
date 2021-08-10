064691ee307464a7bdf8aa698a7ebb780943cb53
kurudrive <kurudrive@gmail.com>
2021-08-11 03:12:36 +0900

    [ G2 ][ 不具合修正 ] ウィジェットプレビューで不要なJSを読み込まないように変更

--- _g2/functions.php
+++ _g2/functions.php
@@ -158,6 +158,9 @@ function lightning_theme_setup() {
 
 add_action( 'wp_enqueue_scripts', 'lightning_addJs' );
 function lightning_addJs() {
+	if ( filter_input( INPUT_GET, 'legacy-widget-preview', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY ) ) {
+		return;
+	}
 	wp_register_script( 'lightning-js', get_template_directory_uri() . '/assets/js/lightning.min.js', array(), LIGHTNING_THEME_VERSION, true );
 	wp_localize_script( 'lightning-js', 'lightningOpt', apply_filters( 'lightning_localize_options', array() ) );
 	// jsのjQuery依存はもう無いが、一応追加しておく
