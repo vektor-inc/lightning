654dc4c5a6224e9ea3c8eb0889312a2dc7d11ebd
drill-lancer <rickaddison7634@gmail.com>
2021-08-19 11:20:39 +0900

    fix: メールチェッカーの見た目

--- inc/admin-mail-checker.php
+++ inc/admin-mail-checker.php
@@ -11,21 +11,21 @@
 function lightning_admin_mail_checker() {
 
 	// 現在の管理者メールアドレス
-	$admin_email  = get_option( 'admin_email' );
+	$admin_email = get_option( 'admin_email' );
 
 	// URL
-	$url          = plugin_dir_url( __FILE__ );
+	$url = plugin_dir_url( __FILE__ );
 
 	// チェックしたい文字列
 	$check_string = 'vektor-inc';
 
 	// メールアドレスを変更する URL
-	$option_link  = admin_url( 'options-general.php' );
+	$option_link = admin_url( 'options-general.php' );
 
 	// 警告文
 	$notice  = '<div class="notice notice-warning"><p>';
 	$notice .= __( 'The administrator email address contains "' . $check_string . '". Please change immediately as you will not receive any inquiries.', 'lightning' );
-	$notice .=' <a href="' . $option_link . '" class="button button-primary">' . __( 'Change email Now!', 'lightning' ) . '</a>';
+	$notice .= '<a href="' . $option_link . '" class="button button-primary">' . __( 'Change email Now!', 'lightning' ) . '</a>';
 	$notice .='</p></div>';
 
 	if (
