ff273834484fd801a26fd9374ffbb666a4b8910c
kurudrive <kurudrive@gmail.com>
2021-09-07 04:08:05 +0900

    phpcs

--- _g2/template-parts/breadcrumb.php
+++ _g2/template-parts/breadcrumb.php
@@ -1,25 +1,33 @@
 <?php
+/**
+ * Lightning Bread Crumb G2
+ *
+ * @package lightning
+ */
 
+/**
+ * Lightning Bread Crumb G2
+ *
+ * @return string html
+ */
 function lightning_bread_crumb() {
-	/*-------------------------------------------*/
-	/*  Lightning BreadCrumb
-	/*-------------------------------------------*/
 
 	global $wp_query;
 
-	// Get Post top page info
-	/*-------------------------------------------*/
+	/********************************************
+	 * Get Post top page info
+	 */
 	// get_post_type() だとtaxonomyページで該当の投稿がない時に投稿タイプを取得できないため lightning_get_post_type() を使用
-	// また、wooCommerceなどはショップトップの名称が投稿タイプ名と異なるので、そのあたりの処理も lightning_get_post_type() で対応済み
-	$post_type_info   = lightning_get_post_type();
-	$show_on_front    = get_option( 'show_on_front' );
-	$page_for_post    = get_option( 'page_for_posts' );
-	$post_top_name    = ! empty( $page_for_post ) ? get_the_title( $page_for_post ) : '';
-	$post_top_url     = isset( $page_for_post ) ? get_permalink( $page_for_post ) : '';
-	$post             = $wp_query->get_queried_object();
+	// また、wooCommerceなどはショップトップの名称が投稿タイプ名と異なるので、そのあたりの処理も lightning_get_post_type() で対応済み.
+	$post_type_info = lightning_get_post_type();
+	$show_on_front  = get_option( 'show_on_front' );
+	$page_for_post  = get_option( 'page_for_posts' );
+	$post_top_name  = ! empty( $page_for_post ) ? get_the_title( $page_for_post ) : '';
+	$post_top_url   = isset( $page_for_post ) ? get_permalink( $page_for_post ) : '';
+	$post           = $wp_query->get_queried_object();
 
 	// Microdata
-	// http://schema.org/BreadcrumbList
+	// https://schema.org/BreadcrumbList ignore:phpcs.
 	/*-------------------------------------------*/
 	$microdata_li        = ' itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"';
 	$microdata_li_a      = ' itemprop="item"';
@@ -37,21 +45,23 @@ function lightning_bread_crumb() {
 	$breadcrumb_html .= '</a>';
 	$breadcrumb_html .= '</li>';
 
-
-	/* Search result
-	/*-------------------------------*/
+	/********************************************
+	 * Search result
+	 */
 	if ( is_search() ) {
 
 		if ( ! empty( get_search_query() ) ) {
+			// translators: seatch keyword.
 			$search_text = sprintf( __( 'Search Results for : %s', 'lightning' ), get_search_query() );
 		} else {
 			$search_text = __( 'Search Results', 'lightning' );
 		}
 		$breadcrumb_html .= '<li><span>' . $search_text . '</span></li>';
 
-	/* Post type
-	/*-------------------------------*/
-	} elseif ( is_single() || is_page() ||is_category() || is_tag() || is_tax() || is_post_type_archive() || is_date() ) {
+		/********************************************
+		 * Post type
+		 */
+	} elseif ( is_single() || is_page() || is_category() || is_tag() || is_tax() || is_post_type_archive() || is_date() ) {
 
 		if ( 'post' === $post_type_info['slug'] && 'page' === $show_on_front && $page_for_post ) { /* including single-post */
 			$breadcrumb_html .= '<li' . $microdata_li . '>';
@@ -71,8 +81,9 @@ function lightning_bread_crumb() {
 			$breadcrumb_html .= '</li>';
 		}
 
-		/* taxonomis list
-		/*-------------------------------*/
+		/********************************************
+		 * Taxonomis list
+		 */
 		if ( is_single() ) {
 			/**
 			 * Single or Page.
@@ -92,8 +103,8 @@ function lightning_bread_crumb() {
 			$exclusion = apply_filters( 'vk_breadcrumb_taxonomies_exludion', $exclusion );
 
 			// タクソノミーの差分を採用.
-			if ( $exclusion ){
-				$taxonomies         = array_diff( $taxonomies, $exclusion );
+			if ( $exclusion ) {
+				$taxonomies = array_diff( $taxonomies, $exclusion );
 			}
 
 			if ( $taxonomies ) {
@@ -110,7 +121,7 @@ function lightning_bread_crumb() {
 					$ancestors = array_reverse( get_ancestors( $term->term_id, $taxonomy ) );
 					// Print loop term ancestors.
 					foreach ( $ancestors as $ancestor ) {
-						$pan_term           = get_term( $ancestor, $taxonomy );
+						$pan_term         = get_term( $ancestor, $taxonomy );
 						$breadcrumb_html .= '<li' . $microdata_li . '>';
 						$breadcrumb_html .= '<a' . $microdata_li_a . ' href="' . get_term_link( $ancestor, $taxonomy ) . '">';
 						$breadcrumb_html .= '<span' . $microdata_li_a_span . '>' . esc_html( $pan_term->name ) . '</span>';
@@ -118,9 +129,9 @@ function lightning_bread_crumb() {
 						$breadcrumb_html .= '</li>';
 					}
 				}
-				$term_url           = get_term_link( $term->term_id, $taxonomy );
+				$term_url         = get_term_link( $term->term_id, $taxonomy );
 				$breadcrumb_html .= '<li' . $microdata_li . '>';
-				$breadcrumb_html .= '<a' . $microdata_li_a . ' href="' .$term_url . '">';
+				$breadcrumb_html .= '<a' . $microdata_li_a . ' href="' . $term_url . '">';
 				$breadcrumb_html .= '<span' . $microdata_li_a_span . '>' . $term->name . '</span>';
 				$breadcrumb_html .= '</a>';
 				$breadcrumb_html .= '</li>';
@@ -143,9 +154,7 @@ function lightning_bread_crumb() {
 
 			// The Single or Page.
 			$breadcrumb_html .= '<li>';
-			// $breadcrumb_html .= '<a' . $microdata_li_a . ' href="' . get_permalink() . '">';
 			$breadcrumb_html .= '<span>' . get_the_title() . '</span>';
-			// $breadcrumb_html .= '</a>';
 			$breadcrumb_html .= '</li>';
 
 		} elseif ( is_category() || is_tag() || is_tax() ) {
@@ -161,7 +170,7 @@ function lightning_bread_crumb() {
 				$ancestors = array_reverse( get_ancestors( $now_term, $now_taxonomy ) );
 				// Parent Taxonomy.
 				foreach ( $ancestors as $ancestor ) {
-					$pan_term           = get_term( $ancestor, $now_taxonomy );
+					$pan_term         = get_term( $ancestor, $now_taxonomy );
 					$breadcrumb_html .= '<li' . $microdata_li . '>';
 					$breadcrumb_html .= '<a' . $microdata_li_a . ' href="' . get_term_link( $ancestor, $now_taxonomy ) . '">';
 					$breadcrumb_html .= '<span' . $microdata_li_a_span . '>' . esc_html( $pan_term->name ) . '</span>';
@@ -170,22 +179,20 @@ function lightning_bread_crumb() {
 				}
 			}
 			$breadcrumb_html .= '<li>';
-			// $breadcrumb_html .= '<a' . $microdata_li_a . ' href="' . get_term_link( $now_term, $now_taxonomy ) . '">';
 			$breadcrumb_html .= '<span>' . esc_html( single_cat_title( '', '', false ) ) . '</span>';
-			// $breadcrumb_html .= '</a>';
 			$breadcrumb_html .= '</li>';
 		} elseif ( is_date() ) {
 			$breadcrumb_html .= '<li>';
 			$breadcrumb_html .= '<span>' . get_the_archive_title() . '</span>';
 			$breadcrumb_html .= '</li>';
 		}
-	}  elseif ( is_home() && ! is_front_page() ) {
+	} elseif ( is_home() && ! is_front_page() ) {
 		$breadcrumb_html .= '<li>';
 		$breadcrumb_html .= '<span>' . $post_top_name . '</span>';
 		$breadcrumb_html .= '</li>';
 	} elseif ( is_author() ) {
-		$author_id          = get_the_author_meta( 'ID' );
-		$author_url         = get_author_posts_url( $author_id );
+		$author_id        = get_the_author_meta( 'ID' );
+		$author_url       = get_author_posts_url( $author_id );
 		$breadcrumb_html .= '<li' . $microdata_li . '>';
 		$breadcrumb_html .= '<a' . $microdata_li_a . ' href="' . $author_url . '">';
 		$breadcrumb_html .= '<span' . $microdata_li_a_span . '>' . get_the_archive_title() . '</span>';
@@ -209,14 +216,14 @@ function lightning_bread_crumb() {
 		// $dynamic_css = trim( $dynamic_css );
 		// convert tab and br to space
 		$breadcrumb_html = preg_replace( '/[\n\r\t]/', '', $breadcrumb_html );
-		// Change multiple spaces to single space
+		// Change multiple spaces to single space.
 		$breadcrumb_html = preg_replace( '/\s(?=\s)/', '', $breadcrumb_html );
 	return $breadcrumb_html;
 }
 
 $breadcrumb_html = lightning_bread_crumb();
 
-$allowed_html = array(
+$allowed_html    = array(
 	'div'  => array(
 		'id'        => array(),
 		'class'     => array(),
@@ -257,6 +264,5 @@ $allowed_html = array(
 		'class' => array(),
 	),
 );
-$breadcrumb_html  = apply_filters( 'lightning_panListHtml', $breadcrumb_html );
-$breadcrumb_html  = wp_kses( $breadcrumb_html, $allowed_html );
-echo $breadcrumb_html;
+$breadcrumb_html = apply_filters( 'lightning_panListHtml', $breadcrumb_html ); // phpcs:ignore
+echo wp_kses( $breadcrumb_html );
