46c3c56267fa7728a83178f41c08812341de157f
kurudrive <kurudrive@gmail.com>
2021-10-23 03:51:32 +0900

    [ G2 ] phpcs

--- _g2/inc/class-design-manager.php
+++ _g2/inc/class-design-manager.php
@@ -1,14 +1,22 @@
-<?php
+<?php // phpcs:ignore
+/**
+ * Lightning_Design_Manager
+ */
 class Lightning_Design_Manager {
 
-	static function init() {
+	/**
+	 * Init functions
+	 *
+	 * @return void
+	 */
+	public static function init() {
 
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'load_skin_css_and_js' ) );
-		// Set to priority 9 that to be load before theme style.css
+		// Set to priority 9 that to be load before theme style.css.
 		add_action( 'after_setup_theme', array( __CLASS__, 'load_late_css_action' ), 9 );
 
 		// Don't use following action point.
-		// wp : Bring to phpunit test error
+		// wp : Bring to phpunit test error.
 		add_action( 'after_setup_theme', array( __CLASS__, 'load_skin_php' ) );
 
 		add_action( 'wp', array( __CLASS__, 'load_skin_callback' ) );
@@ -29,7 +37,7 @@ class Lightning_Design_Manager {
 	 *
 	 * @return array スキン固有で読み込むファイル情報の配列
 	 */
-	static function get_skins() {
+	public static function get_skins() {
 		$skins = array(
 			'origin'  => array(
 				'label'           => __( 'Origin ( Not recommended )', 'lightning' ),
@@ -55,7 +63,7 @@ class Lightning_Design_Manager {
 				'bootstrap'       => 'bs4',
 			),
 		);
-		return apply_filters( 'lightning-design-skins', $skins );
+		return apply_filters( 'lightning-design-skins', $skins ); // phpcs:ignore
 	}
 
 
@@ -64,7 +72,7 @@ class Lightning_Design_Manager {
 	 *
 	 * @return [array] Return plugin url to activate check.
 	 */
-	static function get_skins_info() {
+	public static function get_skins_info() {
 		$skins = array(
 			'variety'               => array(
 				'plugin_path' => 'lightning-skin-variety/lightning_skin_variety.php',
@@ -121,7 +129,7 @@ class Lightning_Design_Manager {
 		}
 
 		// If selected skin plugin is deactive that, set to default skin.
-		if ( $current_skin != 'origin2' ) {
+		if ( 'origin2' !== $current_skin ) {
 			include_once ABSPATH . 'wp-admin/includes/plugin.php';
 			$skins_info = self::get_skins_info();
 			if ( isset( $skins_info[ $current_skin ]['plugin_path'] ) && ! is_plugin_active( $skins_info[ $current_skin ]['plugin_path'] ) ) {
@@ -138,9 +146,9 @@ class Lightning_Design_Manager {
 	/**
 	 * Load skin CSS and JavaScript
 	 *
-	 * @return [type] [description]
+	 * @return void
 	 */
-	static function load_skin_css_and_js() {
+	public static function load_skin_css_and_js() {
 		$skin_info = self::get_current_skin();
 
 		$skin_css_url = '';
@@ -148,39 +156,34 @@ class Lightning_Design_Manager {
 			$skin_css_url = $skin_info['css_path'];
 		}
 
-		// load bootstrap ///////////////////////
+		// load bootstrap ///////////////////////.
 
 		global $bootstrap;
 		$bootstrap = '3';
 		if ( empty( $skin_info['bootstrap'] ) ) {
 			// Bootstrap3 skin
-			// load bootstrap3 js
+			// load bootstrap3 js .
 			wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/library/bootstrap-3/js/bootstrap.min.js', array( 'jquery' ), '3.4.1', true );
 
 			wp_enqueue_style( 'lightning-design-style', $skin_css_url, array(), $skin_info['version'] );
 
-		} elseif ( $skin_info['bootstrap'] == 'bs4' ) {
-			$options = get_option( 'lightning_theme_options' );
+		} elseif ( 'bs4' === $skin_info['bootstrap'] ) {
+			$options   = get_option( 'lightning_theme_options' );
 			$bootstrap = '4';
-			// Bootstrap4 skin
+			// Bootstrap4 skin.
 			$bs4_version = '4.5.0';
 			$bs4_css_url = '';
-			// if ( ! empty( $options['optimize_css'] ) && 'minimal-bootstrap' === $options['optimize_css'] ) {
-			// 	$bs4_css_url = get_template_directory_uri() . '/library/bootstrap-4/css/bootstrap-lite.min.css';
-			// } else {
-				$bs4_css_url = get_template_directory_uri() . '/library/bootstrap-4/css/bootstrap.min.css';
-			// }
+			$bs4_css_url = get_template_directory_uri() . '/library/bootstrap-4/css/bootstrap.min.css';
 			wp_enqueue_style( 'bootstrap-4-style', $bs4_css_url, array(), $bs4_version );
-
 			wp_enqueue_script( 'bootstrap-4-js', get_template_directory_uri() . '/library/bootstrap-4/js/bootstrap.min.js', array( 'jquery' ), $bs4_version, true );
 
-			// load skin CSS ///////////////////////
+			// load skin CSS ///////////////////////.
 
-			wp_enqueue_style( 'lightning-design-style', $skin_css_url, array( 'bootstrap-4-style','lightning-common-style' ), $skin_info['version'] );
+			wp_enqueue_style( 'lightning-design-style', $skin_css_url, array( 'bootstrap-4-style', 'lightning-common-style' ), $skin_info['version'] );
 
 		}
 
-		// load JS ///////////////////////
+		// load JS ///////////////////////.
 
 		if ( ! empty( $skin_info['js_path'] ) ) {
 			wp_enqueue_script( 'lightning-design-js', $skin_info['js_path'], array( 'jquery' ), $skin_info['version'], true );
@@ -198,7 +201,12 @@ class Lightning_Design_Manager {
 		add_action( $hook_point, array( __CLASS__, 'load_late_load_css' ) );
 	}
 
-	static function load_late_load_css() {
+	/**
+	 * Load late css
+	 *
+	 * @return void
+	 */
+	public static function load_late_load_css() {
 		$skin_info = self::get_current_skin();
 		if ( ! empty( $skin_info['css_late_path'] ) ) {
 			$skin_css_footer_url = $skin_info['css_late_path'];
@@ -210,13 +218,13 @@ class Lightning_Design_Manager {
 	/**
 	 * Load skin Editor CSS
 	 *
-	 * @return [type] [description]
+	 * @return void
 	 */
-	static function load_skin_editor_css() {
+	public static function load_skin_editor_css() {
 		$skin_info = self::get_current_skin();
 
-		if ( isset( $skin_info['bootstrap'] ) && $skin_info['bootstrap'] == 'bs4' ) {
-			// Bootstrap4 skin
+		if ( isset( $skin_info['bootstrap'] ) && 'bs4' === $skin_info['bootstrap'] ) {
+			// Bootstrap4 skin.
 			/*
 			 Notice : Use url then if you use local environment https has error that bring to get css error and don't refrected */
 			/* Notice : add_editor_style() is only one args. */
@@ -228,9 +236,12 @@ class Lightning_Design_Manager {
 		}
 	}
 
-	// This method is planned to be removed.
-	// It's aleady don't need function that add_editor_style() become covered gutenberg more better.
-	static function load_skin_gutenberg_css() {
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
+	public static function load_skin_gutenberg_css() {
 		$skin_info = self::get_current_skin();
 		if ( ! empty( $skin_info['gutenberg_css_path'] ) ) {
 			wp_enqueue_style(
@@ -243,21 +254,37 @@ class Lightning_Design_Manager {
 
 	}
 
-	static function load_skin_php() {
+	/**
+	 * Load_skin_php
+	 *
+	 * @return void
+	 */
+	public static function load_skin_php() {
 		$skin_info = self::get_current_skin();
 		if ( ! empty( $skin_info['php_path'] ) && file_exists( $skin_info['php_path'] ) ) {
 			require $skin_info['php_path'];
 		}
 	}
 
-	static function load_skin_callback() {
+	/**
+	 * Load skin callback
+	 *
+	 * @return void
+	 */
+	public static function load_skin_callback() {
 		$skin_info = self::get_current_skin();
-		if ( ! empty( $skin_info['callback'] ) and $skin_info['callback'] ) {
+		if ( ! empty( $skin_info['callback'] ) && $skin_info['callback'] ) {
 			call_user_func_array( $skin_info['callback'], array() );
 		}
 	}
 
-	static function customize_register( $wp_customize ) {
+	/**
+	 * Customize register
+	 *
+	 * @param object $wp_customize : customize object.
+	 * @return void
+	 */
+	public static function customize_register( $wp_customize ) {
 		$wp_customize->add_setting(
 			'lightning_design_skin',
 			array(
