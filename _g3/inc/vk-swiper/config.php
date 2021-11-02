04aee68f3791f5845142a5e1f723b39aaadae992
kurudrive <kurudrive@gmail.com>
2021-11-02 19:36:52 +0900

    [ G3 ][ 仕様変更 ] Swiper Update

--- _g3/inc/vk-swiper/config.php
+++ _g3/inc/vk-swiper/config.php
@@ -18,8 +18,10 @@ if ( ! empty( $options['top_slide_display'] ) && 'hide' === $options['top_slide_
 if ( ! class_exists( 'VK_Swiper' ) ) {
 	global $vk_swiper_url;
 	global $vk_swiper_path;
-	$vk_swiper_url = get_parent_theme_file_uri( 'inc/vk-swiper/package/' );
+	$vk_swiper_url  = get_parent_theme_file_uri( 'inc/vk-swiper/package/' );
 	$vk_swiper_path = get_parent_theme_file_path( 'inc/vk-swiper/package/' );
 	require_once dirname( __FILE__ ) . '/package/class-vk-swiper.php';
+	// Load Swiper js and css (all pages).
+	VK_Swiper::enqueue_swiper();
 }
 
