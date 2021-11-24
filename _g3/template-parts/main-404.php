5349ff7b68a789bd134a707e1f986ac8273d5a4f
kurudrive <kurudrive@gmail.com>
2021-11-24 21:29:20 +0900

    [ 機能追加 ] 404用テンプレートファイル&改変用フック追加

---
+++ _g3/template-parts/main-404.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 404 main template
+ *
+ * @package vektor-inc/lightning
+ */
+?>
+
+<div class="main-section-no-posts">
+	<p>
+		<?php echo __( 'The page you are looking for cannot be found.', 'lightning' ); ?>
+	</p>
+</div>
