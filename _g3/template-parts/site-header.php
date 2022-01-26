1689a3d565970f058c8e0f12d0cf840341cf091d
kurudrive <kurudrive@gmail.com>
2022-01-26 18:20:02 +0900

    G2 / G3 切替時のエラー修正

--- _g3/template-parts/site-header.php
+++ _g3/template-parts/site-header.php
@@ -18,16 +18,20 @@
 		<?php do_action( 'lightning_site_header_logo_after' ); ?>
 
 		<?php
-		wp_nav_menu( array(
-			'theme_location' 	=> 'global-nav',
-			'container'      	=> 'nav',
-			'container_class'	=> lightning_get_the_class_name( 'global-nav' ),
-			'container_id'		=> 'global-nav',
-			'items_wrap'     	=> '<ul id="%1$s" class="%2$s vk-menu-acc global-nav-list nav">%3$s</ul>',
-			'fallback_cb'    	=> '',
-			'echo'           	=> true,
-			'walker'         	=> new VK_Description_Walker(),
-		) );
+		if ( class_exists( 'VK_Description_Walker' ) ) {
+			wp_nav_menu(
+				array(
+					'theme_location'  => 'global-nav',
+					'container'       => 'nav',
+					'container_class' => lightning_get_the_class_name( 'global-nav' ),
+					'container_id'    => 'global-nav',
+					'items_wrap'      => '<ul id="%1$s" class="%2$s vk-menu-acc global-nav-list nav">%3$s</ul>',
+					'fallback_cb'     => '',
+					'echo'            => true,
+					'walker'          => new VK_Description_Walker(),
+				)
+			);
+		}
 		?>
 	</div>
 	<?php do_action( 'lightning_site_header_append' ); ?>
