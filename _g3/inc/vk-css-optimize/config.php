f19702f1f5836eaaf86d2edb178f7cb0443fa21f
kurudrive <kurudrive@gmail.com>
2022-12-21 00:32:48 +0900

    [ CSS最適化 ] 設定ファイル調整

--- _g3/inc/vk-css-optimize/config.php
+++ _g3/inc/vk-css-optimize/config.php
@@ -5,12 +5,11 @@
  * @package Lightning
  */
 
+use VektorInc\VK_CSS_Optimize\VkCssOptimize;
+new VkCssOptimize();
+
 global $prefix_customize_panel;
 $prefix_customize_panel = lightning_get_prefix_customize_panel();
-
-if ( ! class_exists( 'VK_CSS_Optimize' ) ) {
-	require_once dirname( __FILE__ ) . '/package/class-vk-css-optimize.php';
-}
 
 /**
  * Register tree shaking css
@@ -20,28 +19,13 @@ if ( ! class_exists( 'VK_CSS_Optimize' ) ) {
  */
 function lightning_css_tree_shaking_array( $vk_css_tree_shaking_array ) {
 
-	// Register common css.
-	$vk_css_tree_shaking_array[] = array(
-		'id'      => 'lightning-common-style',
-		'url'     => get_template_directory_uri() . '/assets/css/style.css',
-		'path'    => get_parent_theme_file_path() . '/assets/css/style.css',
-		'version' => LIGHTNING_THEME_VERSION,
+	$vk_css_tree_shaking_array = array_merge(
+		$vk_css_tree_shaking_array,
+		array(
+			'lightning-common-style',
+			'lightning-design-style',
+		)
 	);
-
-	// Register design css.
-	$skin_info     = Lightning_Design_Manager::get_current_skin();
-	$skin_css_url  = ! empty( $skin_info['css_url'] ) ? $skin_info['css_url'] : '';
-	$skin_css_path = ! empty( $skin_info['css_path'] ) ? $skin_info['css_path'] : '';
-	$skin_version  = ! empty( $skin_info['version'] ) ? $skin_info['version'] : '';
-
-	if ( ! empty( $skin_css_url ) && ! empty( $skin_version ) ) {
-		$vk_css_tree_shaking_array[] = array(
-			'id'      => 'lightning-design-style',
-			'url'     => $skin_css_url,
-			'path'    => $skin_css_path,
-			'version' => $skin_version,
-		);
-	}
 	return $vk_css_tree_shaking_array;
 }
 add_filter( 'vk_css_tree_shaking_array', 'lightning_css_tree_shaking_array' );
