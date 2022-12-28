30457a265ee1c5921571fe20b50d8e5e76dde3ba
kurudrive <kurudrive@gmail.com>
2022-12-28 21:55:24 +0900

    テスト修正

--- tests/e2e/admin-display.spec.js
+++ tests/e2e/admin-display.spec.js
@@ -8,6 +8,6 @@ test('test', async ({ page }) => {
   await page.getByLabel('Username or Email Address').press('Tab');
   await page.getByLabel('Password').fill('password');
   await page.getByRole('button', { name: 'Log In' }).click();
-  await page.getByText('Themes 0').click();
+  await page.getByRole('link', { name: 'Appearance' }).click();
   await expect(page).toHaveURL('http://localhost:8889/wp-admin/themes.php');
 });
