58052572ef3dcd3e0ca2812c31a0b8c3ce5da35e
kurudrive <kurudrive@gmail.com>
2022-12-28 21:36:30 +0900

    テストURL修正

--- tests/e2e/admin-display.spec.js
+++ tests/e2e/admin-display.spec.js
@@ -2,12 +2,12 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:1122/wp-login.php');
+  await page.goto('http://localhost:8889/wp-login.php');
   await page.getByLabel('Username or Email Address').click();
   await page.getByLabel('Username or Email Address').fill('admin');
   await page.getByLabel('Username or Email Address').press('Tab');
   await page.getByLabel('Password').fill('password');
   await page.getByRole('button', { name: 'Log In' }).click();
   await page.getByText('Themes 0').click();
-  await expect(page).toHaveURL('http://localhost:1122/wp-admin/themes.php');
+  await expect(page).toHaveURL('http://localhost:8889/wp-admin/themes.php');
 });
