1444bd254d37d89a95da61b2bfe2aed003611415
kurudrive <kurudrive@gmail.com>
2023-07-21 19:09:52 +0900

    [ _master.js ] iframe の読み込み改善

--- _g3/assets/_js/_master.js
+++ _g3/assets/_js/_master.js
@@ -106,40 +106,54 @@
         window.addEventListener('document.readyState', header_scrool_func, false)
     }
 
-   /*-------------------------------------------*/
+	/*-------------------------------------------*/
     /*  iframeのレスポンシブ対応
     /*-------------------------------------------*/
-    function iframe_responsive() {
-        Array.prototype.forEach.call(
-            document.getElementsByTagName('iframe'),
-            (i) => {
-                let iframeUrl = i.getAttribute('src')
-                if(!iframeUrl){return}
-                // iframeのURLの中に youtube か map が存在する位置を検索する
-                // 見つからなかった場合には -1 が返される
-                if (
-                    (iframeUrl.indexOf("youtube") >= 0) ||
-                    (iframeUrl.indexOf("vimeo") >= 0) ||
-                    (iframeUrl.indexOf("maps") >= 0)
-                ) {
-                    var iframeWidth = i.getAttribute("width");
-                    var iframeHeight = i.getAttribute("height");
-                    var iframeRate = iframeHeight / iframeWidth;
-                    var nowIframeWidth = i.offsetWidth
-                    var newIframeHeight = nowIframeWidth * iframeRate;
-                    i.style.maxWidth = '100%'
-                    i.style.height = newIframeHeight + 'px'
-                }
-            }
-        );
-    }
-
-    window.addEventListener('DOMContentLoaded',iframe_responsive)
-    let timer = false;
-    window.addEventListener('resize', ()=>{
-        if (timer) clearTimeout(timer);
-        timer = setTimeout(iframe_responsive, 200);
-    })
+	function iframe_responsive() {
+		Array.prototype.forEach.call(
+			document.getElementsByTagName('iframe'),
+			(i) => {
+				let iframeUrl = i.getAttribute('src')
+				if(!iframeUrl){return}
+				// iframeのURLの中に youtube か map が存在する位置を検索する
+				// 見つからなかった場合には -1 が返される
+				if (
+					(iframeUrl.indexOf("youtube") >= 0) ||
+					(iframeUrl.indexOf("vimeo") >= 0) ||
+					(iframeUrl.indexOf("maps") >= 0)
+				) {
+					// iframeの読み込みが完了しているかどうかをチェックする
+					if (i.contentWindow.document.readyState === 'complete') {
+						var iframeWidth = i.getAttribute("width");
+						var iframeHeight = i.getAttribute("height");
+						var iframeRate = iframeHeight / iframeWidth;
+						var nowIframeWidth = i.offsetWidth
+						var newIframeHeight = nowIframeWidth * iframeRate;
+						i.style.maxWidth = '100%'
+						i.style.height = newIframeHeight + 'px'
+					} else {
+						// iframeの読み込みが完了するまで待つ
+						i.contentWindow.document.addEventListener('DOMContentLoaded', () => {
+							var iframeWidth = i.getAttribute("width");
+							var iframeHeight = i.getAttribute("height");
+							var iframeRate = iframeHeight / iframeWidth;
+							var nowIframeWidth = i.offsetWidth
+							var newIframeHeight = nowIframeWidth * iframeRate;
+							i.style.maxWidth = '100%'
+							i.style.height = newIframeHeight + 'px'
+						});
+					}
+				}
+			}
+		);
+	}
+	
+	window.addEventListener('DOMContentLoaded',iframe_responsive)
+	let timer = false;
+	window.addEventListener('resize', ()=>{
+		if (timer) clearTimeout(timer);
+		timer = setTimeout(iframe_responsive, 200);
+	})
 
 
 })(window, document);
