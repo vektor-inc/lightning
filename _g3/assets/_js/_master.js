5e15067dcfce4d2e05ab11e97b0869b20fd48818
kurudrive <kurudrive@gmail.com>
2023-07-21 17:36:15 +0900

    コメント追加のみ

--- _g3/assets/_js/_master.js
+++ _g3/assets/_js/_master.js
@@ -67,29 +67,33 @@
             }, 2000);
         }
 
-        window.addEventListener('DOMContentLoaded', () => {
-            Array.prototype.forEach.call(
-                document.getElementsByTagName('a'),
-                (elem) => {
-                    let href = elem.getAttribute('href')
-					// リンクアドレスの指定が無いか # で始まる場合
-                    if(!href || href.indexOf('#') != 0) return;
-					// role="button" を含めると ボタンブロックのページ内リンクした時にリンク先の頭に固定ナビが上に被ってしまうので tab だけにしている
-                    // if (['tab', 'button'].indexOf(elem.getAttribute('role')) > 0) return;
-                    if (['tab'].indexOf(elem.getAttribute('role')) > 0) return;
-                    if (elem.getAttribute('data-toggle')) return;
-                    if (elem.getAttribute('carousel-control')) return;
-					// スクロール識別クラスを削除する
-                    elem.addEventListener('click', remove_header)
-                }
-            )
-        });
+		// ページ内リンクの場合に固定ヘッダーが被ってしまうのでスクロール識別クラスを削除する
+		window.addEventListener('DOMContentLoaded', () => {
+			// ページ内リンクを持つaタグを取得
+			Array.prototype.forEach.call(
+				document.getElementsByTagName('a'),
+				(elem) => {
+					let href = elem.getAttribute('href')
+
+					// ページ内リンク以外のリンクを無視する
+					if(!href || href.indexOf('#') != 0) return;
+					// role属性があり、属性の値がtabである場合には処理しない
+					// if (['tab', 'button']. のよう button を含めると、ボタンブロックのページ内リンクした時にリンク先の頭に固定ナビが上に被ってしまうので tab だけにしている
+					if (['tab'].indexOf(elem.getAttribute('role')) > 0) return;
+					// data-toggle属性を持つ場合は何もしない
+					if (elem.getAttribute('data-toggle')) return;
+					// carousel-control属性を持つ場合は何もしない
+					if (elem.getAttribute('carousel-control')) return;
+
+					// a タグ（ページ内リンク）をクリックされたらスクロール識別クラスを削除する
+					elem.addEventListener('click', remove_header)
+				}
+			)
+		});
 
         window.addEventListener('scroll', header_scrool_func, true)
         window.addEventListener('DOMContentLoaded', header_scrool_func, false)
     }
-
-
 
    /*-------------------------------------------*/
     /*  iframeのレスポンシブ対応
